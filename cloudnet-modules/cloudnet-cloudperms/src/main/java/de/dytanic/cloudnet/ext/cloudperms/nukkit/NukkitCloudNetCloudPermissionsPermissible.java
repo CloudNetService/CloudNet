0faaa03cf8799a8a35e1a48e3ed897e0f90cb8cc
McRuben <roclaromi@gmail.com>
2019-05-28 14:56:33 +0200

    Implemented Player#getEffectivePermissions on Bukkit and Nukkit

--- cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/nukkit/NukkitCloudNetCloudPermissionsPermissible.java
+++ cloudnet-modules/cloudnet-cloudperms/src/main/java/de/dytanic/cloudnet/ext/cloudperms/nukkit/NukkitCloudNetCloudPermissionsPermissible.java
@@ -3,9 +3,16 @@ package de.dytanic.cloudnet.ext.cloudperms.nukkit;
 import cn.nukkit.Player;
 import cn.nukkit.permission.PermissibleBase;
 import cn.nukkit.permission.Permission;
+import cn.nukkit.permission.PermissionAttachmentInfo;
 import de.dytanic.cloudnet.driver.permission.IPermissionUser;
 import de.dytanic.cloudnet.ext.cloudperms.CloudPermissionsPermissionManagement;
+import de.dytanic.cloudnet.wrapper.Wrapper;
 import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Getter
 public final class NukkitCloudNetCloudPermissionsPermissible extends PermissibleBase {
@@ -17,6 +24,25 @@ public final class NukkitCloudNetCloudPermissionsPermissible extends Permissible
         super(player);
 
         this.player = player;
+    }
+
+    @Override
+    public Map<String, PermissionAttachmentInfo> getEffectivePermissions() {
+        Map<String, PermissionAttachmentInfo> infos = new HashMap<>();
+        IPermissionUser permissionUser = CloudPermissionsPermissionManagement.getInstance().getUser(player.getUniqueId());
+        if (permissionUser == null)
+            return infos;
+
+        for (String group : Wrapper.getInstance().getServiceConfiguration().getGroups()) {
+            infos.putAll(
+                    CloudPermissionsPermissionManagement.getInstance().getAllPermissions(permissionUser, group)
+                            .stream()
+                            .map(permission -> new PermissionAttachmentInfo(this, permission.getName(), null, permission.getPotency() >= 0))
+                            .collect(Collectors.toMap(PermissionAttachmentInfo::getPermission, o -> o))
+            );
+        }
+
+        return infos;
     }
 
     @Override
