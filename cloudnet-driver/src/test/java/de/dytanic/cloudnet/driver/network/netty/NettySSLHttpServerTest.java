65e531fe8fbbda49eb2eb14af0867541332188b7
McRuben <roclaromi@gmail.com>
2019-08-31 16:10:02 +0200

    Revert "Removed all network related tests, they are just causing problems when the ports are already used"
    This reverts commit 78a4d4e9e69f5a8be0e63e745a6a1c0869ec24f7.

---
+++ cloudnet-driver/src/test/java/de/dytanic/cloudnet/driver/network/netty/NettySSLHttpServerTest.java
@@ -0,0 +1,81 @@
+package de.dytanic.cloudnet.driver.network.netty;
+
+import de.dytanic.cloudnet.driver.network.http.HttpResponseCode;
+import de.dytanic.cloudnet.driver.network.http.IHttpHandler;
+import de.dytanic.cloudnet.driver.network.http.IHttpServer;
+import de.dytanic.cloudnet.driver.network.ssl.SSLConfiguration;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+public class NettySSLHttpServerTest {
+
+    @Test
+    public void testSslConfiguration() throws Exception {
+        SelfSignedCertificate selfSignedCertificate = new SelfSignedCertificate();
+
+        try (IHttpServer httpServer = new NettyHttpServer(new SSLConfiguration(
+                false,
+                null,
+                selfSignedCertificate.certificate(),
+                selfSignedCertificate.privateKey()
+        ))) {
+            Assert.assertTrue(httpServer.isSslEnabled());
+            Assert.assertTrue(httpServer.registerHandler("/test/power", (IHttpHandler) (path, context) -> context.response()
+                    .header("Content-Type", "text/plain")
+                    .body("Data-Set")
+                    .statusCode(200)).addListener(32462));
+
+            HttpsURLConnection.setDefaultHostnameVerifier((s, sslSession) -> true);
+
+            TrustManager[] trustManagers = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[]{selfSignedCertificate.cert()};
+                        }
+                    }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustManagers, new SecureRandom());
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection) new URL("https://127.0.0.1:32462/test/power").openConnection();
+            httpURLConnection.connect();
+
+            Assert.assertEquals(HttpResponseCode.HTTP_OK, httpURLConnection.getResponseCode());
+
+            try (InputStream inputStream = httpURLConnection.getInputStream();
+                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                Assert.assertEquals("Data-Set", bufferedReader.readLine());
+            }
+
+            httpURLConnection.disconnect();
+        }
+    }
+}
---
+++ cloudnet-driver/src/test/java/de/dytanic/cloudnet/driver/network/netty/NettyWebSocketServerExample.java
@@ -0,0 +1,132 @@
+package de.dytanic.cloudnet.driver.network.netty;
+
+import de.dytanic.cloudnet.common.concurrent.ITask;
+import de.dytanic.cloudnet.common.concurrent.ListenableTask;
+import de.dytanic.cloudnet.driver.network.HostAndPort;
+import de.dytanic.cloudnet.driver.network.http.IHttpServer;
+import de.dytanic.cloudnet.driver.network.http.websocket.IWebSocketChannel;
+import de.dytanic.cloudnet.driver.network.http.websocket.WebSocketFrameType;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+public final class NettyWebSocketServerExample {
+
+    private static final String
+            PING_STRING = "ping value",
+            PONG_STRING = "pong value",
+            TEXT_STRING = "text value",
+            BINARY_STRING = "binary value";
+
+    private static final ITask<String> task = new ListenableTask<>(() -> "Async_response_message");
+
+    @Test
+    public void testWebSocket() throws Exception {
+        IHttpServer httpServer = new NettyHttpServer();
+
+        httpServer.registerHandler("/test", (path, context) -> {
+            IWebSocketChannel channel = context.upgrade();
+            channel.addListener((channel1, type, bytes) -> {
+                switch (type) {
+                    case PING:
+                        if (new String(bytes, StandardCharsets.UTF_8).equals(PING_STRING)) {
+                            channel1.sendWebSocketFrame(WebSocketFrameType.PONG, PONG_STRING);
+                        }
+                        break;
+                    case TEXT:
+                        if (new String(bytes, StandardCharsets.UTF_8).equals(TEXT_STRING)) {
+                            channel1.sendWebSocketFrame(WebSocketFrameType.BINARY, BINARY_STRING);
+                        }
+                        break;
+                    case CLOSE:
+                        channel1.close();
+                        break;
+                }
+            });
+
+            context.cancelNext();
+        }).addListener(new HostAndPort("0.0.0.0", 4044));
+
+        EventLoopGroup eventLoopGroup = NettyUtils.newEventLoopGroup();
+        WebSocketClientHandshaker webSocketClientHandshaker = WebSocketClientHandshakerFactory
+                .newHandshaker(
+                        new URI("ws://127.0.0.1:4044/test"),
+                        WebSocketVersion.V13,
+                        null,
+                        false,
+                        HttpHeaders.EMPTY_HEADERS,
+                        1280000
+                );
+
+        new Bootstrap()
+                .group(eventLoopGroup)
+                .channel(NettyUtils.getSocketChannelClass())
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ch.pipeline().addLast(
+                                new HttpClientCodec(),
+                                new HttpObjectAggregator(65536),
+                                WebSocketClientCompressionHandler.INSTANCE,
+                                new ChannelInboundHandlerAdapter() {
+
+                                    @Override
+                                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                        webSocketClientHandshaker.handshake(ctx.channel());
+                                    }
+
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        if (!webSocketClientHandshaker.isHandshakeComplete() && msg instanceof FullHttpResponse) {
+                                            try {
+                                                webSocketClientHandshaker.finishHandshake(ctx.channel(), (FullHttpResponse) msg);
+                                            } catch (Exception ex) {
+                                                ex.printStackTrace();
+                                            }
+
+                                            ctx.channel().eventLoop().execute(() -> ctx.channel().writeAndFlush(new PingWebSocketFrame(Unpooled.buffer().writeBytes(PING_STRING.getBytes()))));
+                                            return;
+                                        }
+
+                                        if (msg instanceof WebSocketFrame) {
+                                            WebSocketFrame webSocketFrame = (WebSocketFrame) msg;
+
+                                            if (msg instanceof PongWebSocketFrame) {
+                                                if (PONG_STRING.equals(webSocketFrame.content().toString(StandardCharsets.UTF_8))) {
+                                                    ctx.channel().writeAndFlush(new TextWebSocketFrame(Unpooled.buffer().writeBytes(TEXT_STRING.getBytes())));
+                                                }
+                                                return;
+                                            }
+
+                                            if (msg instanceof BinaryWebSocketFrame) {
+                                                if (BINARY_STRING.equals(webSocketFrame.content().toString(StandardCharsets.UTF_8))) {
+                                                    ctx.channel().writeAndFlush(new CloseWebSocketFrame()).addListener(ChannelFutureListener.CLOSE);
+                                                    task.call();
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
+                    }
+                })
+                .connect("127.0.0.1", 4044)
+                .sync()
+                .channel()
+        ;
+
+        Assert.assertEquals("Async_response_message", task.get());
+        eventLoopGroup.shutdownGracefully();
+        httpServer.close();
+    }
+}
