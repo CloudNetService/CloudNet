372f2e6971ceddb97b12e48e0825cf3d4e55654f
McRuben <37866812+McRuben@users.noreply.github.com>
2019-08-31 19:50:33 +0200

    Fixed warnings/possible errors (#92)
    * Fixed warnings because Optional#get == null was used
    
    * Use the features of the Optional

--- cloudnet-modules/cloudnet-bridge/src/main/java/de/dytanic/cloudnet/ext/bridge/velocity/command/CommandHub.java
+++ cloudnet-modules/cloudnet-bridge/src/main/java/de/dytanic/cloudnet/ext/bridge/velocity/command/CommandHub.java
@@ -3,12 +3,15 @@ package de.dytanic.cloudnet.ext.bridge.velocity.command;
 import com.velocitypowered.api.command.Command;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ServerConnection;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import de.dytanic.cloudnet.ext.bridge.BridgeConfigurationProvider;
 import de.dytanic.cloudnet.ext.bridge.velocity.VelocityCloudNetHelper;
 import net.kyori.text.TextComponent;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.optional.qual.MaybePresent;
+
+import java.util.Optional;
 
 public final class CommandHub implements Command {
 
@@ -25,24 +28,28 @@ public final class CommandHub implements Command {
             return;
         }
 
-        String server = VelocityCloudNetHelper.filterServiceForPlayer(player, player.getCurrentServer().get().getServerInfo().getName());
-
-        if (server != null) {
-            RegisteredServer registeredServer = VelocityCloudNetHelper.getProxyServer().getServer(server).get();
-
-            if (registeredServer == null) {
-                source.sendMessage(TextComponent.of(BridgeConfigurationProvider.load().getMessages().get("command-hub-no-server-found").replace("&", "§")));
-                return;
-            }
-
-            player.createConnectionRequest(registeredServer).connect();
-            source.sendMessage(TextComponent.of(
-                    BridgeConfigurationProvider.load().getMessages().get("command-hub-success-connect")
-                            .replace("%server%", server + "")
-                            .replace("&", "§")
-            ));
-        } else {
+        Optional<ServerConnection> serverConnection = player.getCurrentServer();
+        if (!serverConnection.isPresent()) {
             source.sendMessage(TextComponent.of(BridgeConfigurationProvider.load().getMessages().get("command-hub-no-server-found").replace("&", "§")));
+            return;
         }
+        String server = VelocityCloudNetHelper.filterServiceForPlayer(player, serverConnection.get().getServerInfo().getName());
+
+        if (server == null) {
+            source.sendMessage(TextComponent.of(BridgeConfigurationProvider.load().getMessages().get("command-hub-no-server-found").replace("&", "§")));
+            return;
+        }
+
+        Optional<RegisteredServer> registeredServer = VelocityCloudNetHelper.getProxyServer().getServer(server);
+        if (!registeredServer.isPresent()) {
+            source.sendMessage(TextComponent.of(BridgeConfigurationProvider.load().getMessages().get("command-hub-no-server-found").replace("&", "§")));
+            return;
+        }
+        player.createConnectionRequest(registeredServer.get()).connect();
+        source.sendMessage(TextComponent.of(
+                BridgeConfigurationProvider.load().getMessages().get("command-hub-success-connect")
+                        .replace("%server%", server + "")
+                        .replace("&", "§")
+        ));
     }
 }
