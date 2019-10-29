995f4f6fe0484760d066123c5e75b8388cfbe8cc
derrop <roclaromi@gmail.com>
2019-10-29 21:41:43 +0100

    Fixed wrong console color output

--- cloudnet/src/main/java/de/dytanic/cloudnet/console/ConsoleColor.java
+++ cloudnet/src/main/java/de/dytanic/cloudnet/console/ConsoleColor.java
@@ -38,7 +38,7 @@ public enum ConsoleColor {
         }
 
         for (ConsoleColor consoleColour : values()) {
-            text = text.replace(String.valueOf(triggerChar + consoleColour.index), String.valueOf(consoleColour.ansiCode));
+            text = text.replace(triggerChar + "" + consoleColour.index, consoleColour.ansiCode);
         }
 
         return text;
