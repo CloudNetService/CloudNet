27bd19cfea0b088f11c4c5e4b8b4c49eb15b95ce
derrop <derrop@outlook.de>
2020-01-31 16:23:07 +0100

    Add documentation to some cluster-only events

--- cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/event/events/service/CloudServiceRegisterEvent.java
+++ cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/event/events/service/CloudServiceRegisterEvent.java
@@ -2,6 +2,10 @@ package de.dytanic.cloudnet.driver.event.events.service;
 
 import de.dytanic.cloudnet.driver.service.ServiceInfoSnapshot;
 
+/**
+ * This event will be called when a service is registered.
+ * It will NEVER be called for local services on a node.
+ */
 public final class CloudServiceRegisterEvent extends CloudServiceEvent {
 
     public CloudServiceRegisterEvent(ServiceInfoSnapshot serviceInfo) {
--- cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/event/events/service/CloudServiceStopEvent.java
+++ cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/event/events/service/CloudServiceStopEvent.java
@@ -2,6 +2,10 @@ package de.dytanic.cloudnet.driver.event.events.service;
 
 import de.dytanic.cloudnet.driver.service.ServiceInfoSnapshot;
 
+/**
+ * This event will be called when a service is stopped.
+ * It will NEVER be called for local services on a node.
+ */
 public final class CloudServiceStopEvent extends CloudServiceEvent {
 
     public CloudServiceStopEvent(ServiceInfoSnapshot serviceInfo) {
