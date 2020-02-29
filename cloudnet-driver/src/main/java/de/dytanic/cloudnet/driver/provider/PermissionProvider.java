30110da728f1c18afd91af50ca05949a7701b4ff
derrop <derrop@outlook.de>
2020-02-29 20:37:20 +0100

    Add documentation to the PermissionProvider

--- cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/provider/PermissionProvider.java
+++ cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/provider/PermissionProvider.java
@@ -12,82 +12,312 @@ import java.util.UUID;
 
 public interface PermissionProvider {
 
+    /**
+     * Adds a new user to the database.
+     *
+     * @param permissionUser the user to be added
+     */
     void addUser(@NotNull IPermissionUser permissionUser);
 
+    /**
+     * Updates an already existing user in the database.
+     *
+     * @param permissionUser the user to be updated
+     */
     void updateUser(@NotNull IPermissionUser permissionUser);
 
+    /**
+     * Deletes all users in the database matching the given name.
+     * This method is case-sensitive.
+     *
+     * @param name the name of the users to be deleted
+     */
     void deleteUser(@NotNull String name);
 
+    /**
+     * Deletes one user with the uniqueId of the given user.
+     *
+     * @param permissionUser the user to be deleted
+     */
     void deleteUser(@NotNull IPermissionUser permissionUser);
 
+    /**
+     * Checks if a user with the given uniqueId is stored in the database.
+     *
+     * @param uniqueId the uniqueId of the user
+     * @return {@code true} if there is a user with that uniqueId, {@code false} otherwise
+     */
     boolean containsUser(@NotNull UUID uniqueId);
 
+    /**
+     * Checks if at least one user with the given name is stored in the database.
+     * This method is case-sensitive.
+     *
+     * @param name the name of the user
+     * @return {@code true} if there is a user with that name, {@code false} otherwise
+     */
     boolean containsUser(@NotNull String name);
 
+    /**
+     * Gets a user with the given uniqueId out of the database.
+     *
+     * @param uniqueId the uniqueId of the user
+     * @return the {@link IPermissionUser} from the database or {@code null} if there is no user with that uniqueId stored
+     */
     @Nullable
     IPermissionUser getUser(@NotNull UUID uniqueId);
 
+    /**
+     * Gets a list of all users with the given name out of the database.
+     * This can only return null when the connection to the database (or when it is executed in a
+     * Wrapper instance the connection to the cloud) times out.
+     *
+     * @param name the name of the users
+     * @return a list of all {@link IPermissionUser}s stored in the database or an empty list if there is no user with that name stored.
+     */
     List<IPermissionUser> getUsers(@NotNull String name);
 
+    /**
+     * Gets a list of all users stored in the database.
+     * This can only return null when the connection to the database (or when it is executed in a
+     * Wrapper instance the connection to the cloud) times out.
+     * <p>
+     * This method shouldn't be used when there are many users stored in the database, because that takes a lot of memory.
+     *
+     * @return a list of all {@link IPermissionUser}s stored in the database or an empty list if there is no user with that name stored.
+     */
     Collection<IPermissionUser> getUsers();
 
+    /**
+     * Clears all users stored in the database and inserts the given list.
+     *
+     * @param users the new {@link IPermissionUser}s to be stored in the database
+     */
     void setUsers(@NotNull Collection<? extends IPermissionUser> users);
 
+    /**
+     * Gets a list of all users stored in the database with the given group.
+     * This can only return null when the connection to the database (or when it is executed in a
+     * Wrapper instance the connection to the cloud) times out.
+     * <p>
+     * This method shouldn't be used when there are many users with that group stored in the database, because that takes a lot of memory.
+     *
+     * @return a list of all {@link IPermissionUser}s stored in the database or an empty list if there is no user with that name stored.
+     */
     Collection<IPermissionUser> getUsersByGroup(@NotNull String group);
 
+    /**
+     * Adds a new permission group to the list of groups. If a group with that name already exists,
+     * it will be deleted and created again.
+     *
+     * @param permissionGroup the {@link IPermissionGroup} to be added
+     */
     void addGroup(@NotNull IPermissionGroup permissionGroup);
 
+    /**
+     * Updates a permission group in the list of groups. If a group with that name doesn't exist,
+     * it will be created.
+     *
+     * @param permissionGroup the {@link IPermissionGroup} to be updated
+     */
     void updateGroup(@NotNull IPermissionGroup permissionGroup);
 
+    /**
+     * Deletes a group by its name out of the list of groups. If a group with that name doesn't exist, nothing happens.
+     *
+     * @param name the case-sensitive name of the group
+     */
     void deleteGroup(@NotNull String name);
 
+    /**
+     * Deletes a group by its name out of the list of groups. If a group with that name doesn't exist, nothing happens.
+     *
+     * @param permissionGroup the {@link IPermissionGroup} to be deleted
+     */
     void deleteGroup(@NotNull IPermissionGroup permissionGroup);
 
+    /**
+     * Checks if a specific group exists.
+     *
+     * @param group the case-sensitive name of the group
+     * @return {@code true} if the group exists, {@code false} otherwise
+     */
     boolean containsGroup(@NotNull String group);
 
+    /**
+     * Gets a specific group by its name.
+     *
+     * @param name the case-sensitive name of the group
+     * @return the {@link IPermissionUser} if it exists, {@code null} otherwise
+     */
     @Nullable
     IPermissionGroup getGroup(@NotNull String name);
 
+    /**
+     * Gets the list of all groups in the Cloud.
+     *
+     * @return a list of {@link IPermissionGroup}s registered in the cloud or an empty list if there is no group registered
+     */
     Collection<IPermissionGroup> getGroups();
 
+    /**
+     * Clears all groups in the Cloud and sets given groups.
+     *
+     * @param groups the new groups
+     */
     void setGroups(@NotNull Collection<? extends IPermissionGroup> groups);
 
+    /**
+     * Adds a new user to the database.
+     *
+     * @param permissionUser the user to be added
+     */
     ITask<Void> addUserAsync(@NotNull IPermissionUser permissionUser);
 
+    /**
+     * Updates an already existing user in the database.
+     *
+     * @param permissionUser the user to be updated
+     */
     ITask<Void> updateUserAsync(@NotNull IPermissionUser permissionUser);
 
+    /**
+     * Deletes all users in the database matching the given name.
+     * This method is case-sensitive.
+     *
+     * @param name the name of the users to be deleted
+     */
     ITask<Void> deleteUserAsync(@NotNull String name);
 
+    /**
+     * Deletes one user with the uniqueId of the given user.
+     *
+     * @param permissionUser the user to be deleted
+     */
     ITask<Void> deleteUserAsync(@NotNull IPermissionUser permissionUser);
 
+    /**
+     * Checks if a user with the given uniqueId is stored in the database.
+     *
+     * @param uniqueId the uniqueId of the user
+     * @return {@code true} if there is a user with that uniqueId, {@code false} otherwise
+     */
     ITask<Boolean> containsUserAsync(@NotNull UUID uniqueId);
 
+    /**
+     * Checks if at least one user with the given name is stored in the database.
+     * This method is case-sensitive.
+     *
+     * @param name the name of the user
+     * @return {@code true} if there is a user with that name, {@code false} otherwise
+     */
     ITask<Boolean> containsUserAsync(@NotNull String name);
 
+    /**
+     * Gets a user with the given uniqueId out of the database.
+     *
+     * @param uniqueId the uniqueId of the user
+     * @return the {@link IPermissionUser} from the database or {@code null} if there is no user with that uniqueId stored
+     */
     ITask<IPermissionUser> getUserAsync(@NotNull UUID uniqueId);
 
+    /**
+     * Gets a list of all users with the given name out of the database.
+     * This can only return null when the connection to the database (or when it is executed in a
+     * Wrapper instance the connection to the cloud) times out.
+     *
+     * @param name the name of the users
+     * @return a list of all {@link IPermissionUser}s stored in the database or an empty list if there is no user with that name stored.
+     */
     ITask<List<IPermissionUser>> getUsersAsync(@NotNull String name);
 
+    /**
+     * Gets a list of all users stored in the database.
+     * This can only return null when the connection to the database (or when it is executed in a
+     * Wrapper instance the connection to the cloud) times out.
+     * <p>
+     * This method shouldn't be used when there are many users stored in the database, because that takes a lot of memory.
+     *
+     * @return a list of all {@link IPermissionUser}s stored in the database or an empty list if there is no user with that name stored.
+     */
     ITask<Collection<IPermissionUser>> getUsersAsync();
 
+    /**
+     * Clears all users stored in the database and inserts the given list.
+     *
+     * @param users the new {@link IPermissionUser}s to be stored in the database
+     */
     ITask<Void> setUsersAsync(@NotNull Collection<? extends IPermissionUser> users);
 
+    /**
+     * Gets a list of all users stored in the database with the given group.
+     * This can only return null when the connection to the database (or when it is executed in a
+     * Wrapper instance the connection to the cloud) times out.
+     * <p>
+     * This method shouldn't be used when there are many users with that group stored in the database, because that takes a lot of memory.
+     *
+     * @return a list of all {@link IPermissionUser}s stored in the database or an empty list if there is no user with that name stored.
+     */
     ITask<Collection<IPermissionUser>> getUsersByGroupAsync(@NotNull String group);
 
+    /**
+     * Adds a new permission group to the list of groups. If a group with that name already exists,
+     * it will be deleted and created again.
+     *
+     * @param permissionGroup the {@link IPermissionGroup} to be added
+     */
     ITask<Void> addGroupAsync(@NotNull IPermissionGroup permissionGroup);
 
+    /**
+     * Updates a permission group in the list of groups. If a group with that name doesn't exist,
+     * it will be created.
+     *
+     * @param permissionGroup the {@link IPermissionGroup} to be updated
+     */
     ITask<Void> updateGroupAsync(@NotNull IPermissionGroup permissionGroup);
 
+    /**
+     * Deletes a group by its name out of the list of groups. If a group with that name doesn't exist, nothing happens.
+     *
+     * @param name the case-sensitive name of the group
+     */
     ITask<Void> deleteGroupAsync(@NotNull String name);
 
+    /**
+     * Deletes a group by its name out of the list of groups. If a group with that name doesn't exist, nothing happens.
+     *
+     * @param permissionGroup the {@link IPermissionGroup} to be deleted
+     */
     ITask<Void> deleteGroupAsync(@NotNull IPermissionGroup permissionGroup);
 
-    ITask<Boolean> containsGroupAsync(@NotNull String name);
+    /**
+     * Checks if a specific group exists.
+     *
+     * @param group the case-sensitive name of the group
+     * @return {@code true} if the group exists, {@code false} otherwise
+     */
+    ITask<Boolean> containsGroupAsync(@NotNull String group);
 
+    /**
+     * Gets a specific group by its name.
+     *
+     * @param name the case-sensitive name of the group
+     * @return the {@link IPermissionUser} if it exists, {@code null} otherwise
+     */
     ITask<IPermissionGroup> getGroupAsync(@NotNull String name);
 
+    /**
+     * Gets the list of all groups in the Cloud.
+     *
+     * @return a list of {@link IPermissionGroup}s registered in the cloud or an empty list if there is no group registered
+     */
     ITask<Collection<IPermissionGroup>> getGroupsAsync();
 
+    /**
+     * Clears all groups in the Cloud and sets given groups.
+     *
+     * @param groups the new groups
+     */
     ITask<Void> setGroupsAsync(@NotNull Collection<? extends IPermissionGroup> groups);
 
 }
