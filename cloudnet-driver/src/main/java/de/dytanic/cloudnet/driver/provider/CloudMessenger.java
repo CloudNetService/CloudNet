aec7ca4e2f7627ca0a3d529b8be56bcb26bf150f
derrop <derrop@outlook.de>
2020-03-01 01:35:53 +0100

    Add documentation to the CloudMessenger

--- cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/provider/CloudMessenger.java
+++ cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/provider/CloudMessenger.java
@@ -1,16 +1,44 @@
 package de.dytanic.cloudnet.driver.provider;
 
 import de.dytanic.cloudnet.common.document.gson.JsonDocument;
+import de.dytanic.cloudnet.driver.event.events.channel.ChannelMessageReceiveEvent;
 import de.dytanic.cloudnet.driver.service.ServiceInfoSnapshot;
 import de.dytanic.cloudnet.driver.service.ServiceTask;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A messenger to communicate between services in CloudNet.
+ */
 public interface CloudMessenger {
 
+    /**
+     * Sends a channel message to all services in the cluster.
+     * It can be received with the {@link ChannelMessageReceiveEvent}.
+     *
+     * @param channel the channel to identify the message, this can be anything and doesn't have to be registered
+     * @param message the message to identify the message, this can be anything and doesn't have to be registered
+     * @param data    extra data for the message
+     */
     void sendChannelMessage(@NotNull String channel, @NotNull String message, @NotNull JsonDocument data);
 
+    /**
+     * Sends a channel message to a specific service in the cluster.
+     * It can be received with the {@link ChannelMessageReceiveEvent}.
+     *
+     * @param channel the channel to identify the message, this can be anything and doesn't have to be registered
+     * @param message the message to identify the message, this can be anything and doesn't have to be registered
+     * @param data    extra data for the message
+     */
     void sendChannelMessage(@NotNull ServiceInfoSnapshot targetServiceInfoSnapshot, @NotNull String channel, @NotNull String message, @NotNull JsonDocument data);
 
+    /**
+     * Sends a channel message to all services of a specific task in the cluster.
+     * It can be received with the {@link ChannelMessageReceiveEvent}.
+     *
+     * @param channel the channel to identify the message, this can be anything and doesn't have to be registered
+     * @param message the message to identify the message, this can be anything and doesn't have to be registered
+     * @param data    extra data for the message
+     */
     void sendChannelMessage(@NotNull ServiceTask targetServiceTask, @NotNull String channel, @NotNull String message, @NotNull JsonDocument data);
     
 }
