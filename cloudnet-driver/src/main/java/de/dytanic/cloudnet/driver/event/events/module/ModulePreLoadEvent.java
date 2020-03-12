d71cd4062d22667560ae791952fa1831a85e5588
Pasqual K <pasqual.koschmieder@gmail.com>
2020-03-12 21:28:22 +0100

    Made module pre events cancelable

--- cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/event/events/module/ModulePreLoadEvent.java
+++ cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/event/events/module/ModulePreLoadEvent.java
@@ -1,11 +1,24 @@
 package de.dytanic.cloudnet.driver.event.events.module;
 
+import de.dytanic.cloudnet.driver.event.ICancelable;
 import de.dytanic.cloudnet.driver.module.IModuleProvider;
 import de.dytanic.cloudnet.driver.module.IModuleWrapper;
 
-public final class ModulePreLoadEvent extends ModuleEvent {
+public final class ModulePreLoadEvent extends ModuleEvent implements ICancelable {
 
     public ModulePreLoadEvent(IModuleProvider moduleProvider, IModuleWrapper module) {
         super(moduleProvider, module);
+    }
+
+    private boolean isCancelled;
+
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean value) {
+        this.isCancelled = value;
     }
 }
