53bac54cd285d5d2be5fe11bdbd1eaeb07a97847
Julian <42209193+juliarn@users.noreply.github.com>
2020-03-22 19:57:10 +0100

    Fix autoDelete/autoPrepare of services Replace functions in enums with abstract methods

--- cloudnet-modules/cloudnet-storage-ftp/src/main/java/de/dytanic/cloudnet/ext/storage/ftp/client/FTPType.java
+++ cloudnet-modules/cloudnet-storage-ftp/src/main/java/de/dytanic/cloudnet/ext/storage/ftp/client/FTPType.java
@@ -4,21 +4,27 @@ import de.dytanic.cloudnet.ext.storage.ftp.storage.AbstractFTPStorage;
 import de.dytanic.cloudnet.ext.storage.ftp.storage.FTPTemplateStorage;
 import de.dytanic.cloudnet.ext.storage.ftp.storage.SFTPTemplateStorage;
 
-import java.util.function.BiFunction;
-
 public enum FTPType {
-    SFTP(SFTPTemplateStorage::new),
-    FTP((name, credentials) -> new FTPTemplateStorage(name, credentials, false)),
-    FTPS((name, credentials) -> new FTPTemplateStorage(name, credentials, true));
+    SFTP {
+        @Override
+        public AbstractFTPStorage createNewTemplateStorage(String storage, FTPCredentials credentials) {
+            return new SFTPTemplateStorage(storage, credentials);
+        }
+    },
+    FTP {
+        @Override
+        public AbstractFTPStorage createNewTemplateStorage(String storage, FTPCredentials credentials) {
+            return new FTPTemplateStorage(storage, credentials, false);
+        }
+    },
+    FTPS {
+        @Override
+        public AbstractFTPStorage createNewTemplateStorage(String storage, FTPCredentials credentials) {
+            return new FTPTemplateStorage(storage, credentials, true);
+        }
+    };
 
-    private BiFunction<String, FTPCredentials, AbstractFTPStorage> storageProvider;
 
-    FTPType(BiFunction<String, FTPCredentials, AbstractFTPStorage> storageProvider) {
-        this.storageProvider = storageProvider;
-    }
-
-    public AbstractFTPStorage createNewTemplateStorage(String storage, FTPCredentials credentials) {
-        return this.storageProvider.apply(storage, credentials);
-    }
+    public abstract AbstractFTPStorage createNewTemplateStorage(String storage, FTPCredentials credentials);
 
 }
