28248aa6c213df8f329740f081e1f632a02d050d
derrop <derrop@outlook.de>
2020-03-23 19:20:55 +0100

    Add documentation to the ServiceProperty

--- cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/service/property/ServiceProperty.java
+++ cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/service/property/ServiceProperty.java
@@ -6,11 +6,29 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
+/**
+ * This class helps getting/modifying properties in {@link ServiceInfoSnapshot}s.
+ *
+ * @param <T> the type of the value for this property
+ */
 public interface ServiceProperty<T> {
 
+    /**
+     * Gets a property out of the properties of the given {@link ServiceInfoSnapshot}.
+     *
+     * @param serviceInfoSnapshot the serviceInfoSnapshot to get the property
+     * @return an optional with the value, might be empty
+     */
     @NotNull
     Optional<T> get(@NotNull ServiceInfoSnapshot serviceInfoSnapshot);
 
+    /**
+     * Sets a property into the properties of the given {@link ServiceInfoSnapshot}. Some properties might not support this.
+     * An update of the service might be necessary.
+     *
+     * @param serviceInfoSnapshot the serviceInfoSnapshot to modify the property in
+     * @param value               the value to set or null to remove the property
+     */
     void set(@NotNull ServiceInfoSnapshot serviceInfoSnapshot, @Nullable T value);
 
 }
