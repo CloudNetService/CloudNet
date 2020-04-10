b6eb484b9c93ca4fa8ff869d17585e1f52a7d8ab
derrop <derrop@outlook.de>
2020-04-11 00:44:21 +0200

    Remove twice calling of the task listeners in the ListenableTask

--- cloudnet-common/src/main/java/de/dytanic/cloudnet/common/concurrent/ListenableTask.java
+++ cloudnet-common/src/main/java/de/dytanic/cloudnet/common/concurrent/ListenableTask.java
@@ -137,7 +137,6 @@ public class ListenableTask<V> implements ITask<V> {
                 this.value = this.callable.call();
             } catch (Throwable throwable) {
                 this.throwable = throwable;
-                this.invokeTaskListener();
             }
         }
 
