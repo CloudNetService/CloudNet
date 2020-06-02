d4dbaa1c7ffbefa6b53b39c946c7cfe9a1dfa8bd
derrop <derrop@outlook.de>
2020-06-02 16:30:13 +0200

    Update ChannelMessaging example

--- cloudnet-examples/src/main/java/de/dytanic/cloudnet/examples/ExampleChannelPublishAndSubscribe.java
+++ cloudnet-examples/src/main/java/de/dytanic/cloudnet/examples/ExampleChannelPublishAndSubscribe.java
@@ -1,22 +1,40 @@
 package de.dytanic.cloudnet.examples;
 
 import de.dytanic.cloudnet.common.document.gson.JsonDocument;
-import de.dytanic.cloudnet.driver.CloudNetDriver;
+import de.dytanic.cloudnet.driver.channel.ChannelMessage;
 import de.dytanic.cloudnet.driver.event.EventListener;
 import de.dytanic.cloudnet.driver.event.events.channel.ChannelMessageReceiveEvent;
 
 public final class ExampleChannelPublishAndSubscribe {
 
-    public void publishMessage() {
-        CloudNetDriver.getInstance().getMessenger().sendChannelMessage("user_channel", "user_info_publishing", new JsonDocument()
-                .append("name", "Peter Parker")
-                .append("age", 17)
-        );
+    public void publishGlobalMessage() {
+        ChannelMessage.builder()
+                .channel("user_channel")
+                .message("user_info_publishing")
+                .jsonContent(JsonDocument.newDocument().append("name", "Peter Parker").append("age", 17))
+                .targetAll()
+                .build()
+                .send();
         //Send a channel message to all services and nodes in network
+    }
+
+    public void publishMessage(String targetServiceName) {
+        ChannelMessage.builder()
+                .channel("user_channel")
+                .message("user_info_publishing")
+                .jsonContent(JsonDocument.newDocument().append("name", "Peter Parker").append("age", 17))
+                .targetService(targetServiceName)
+                .build()
+                .send();
+        //Send a channel message to a specified service
     }
 
     @EventListener
     public void handleChannelMessage(ChannelMessageReceiveEvent event) {
+        if (event.getMessage() == null || event.getData() == null) {
+            return;
+        }
+
         if (event.getChannel().equalsIgnoreCase("user_channel")) {
             if ("user_info_publishing".equals(event.getMessage().toLowerCase())) {
                 System.out.println("Name: " + event.getData().getString("name") + " | Age: " + event.getData().getInt("age"));
