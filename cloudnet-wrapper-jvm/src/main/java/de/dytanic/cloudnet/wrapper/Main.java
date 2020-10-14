470142ce8b53557fc9dccfac877e9bbbf72f1893
Arnaud Lier <zeprofdecoding@gmail.com>
2020-10-14 22:52:02 +0200

    Create french.properties (#367)
    * Create french.properties
    
    * Create french.properties
    
    * Update Main.java
    
    * Update Main.java
    
    * Update french.properties
    
    * Remove spaces in front of punctuation marks
    
    Co-authored-by: Julian <42209193+juliarn@users.noreply.github.com>

--- cloudnet-wrapper-jvm/src/main/java/de/dytanic/cloudnet/wrapper/Main.java
+++ cloudnet-wrapper-jvm/src/main/java/de/dytanic/cloudnet/wrapper/Main.java
@@ -21,6 +21,7 @@ public final class Main {
         LanguageManager.setLanguage(System.getProperty("cloudnet.wrapper.messages.language", "english"));
         LanguageManager.addLanguageFile("german", Main.class.getClassLoader().getResourceAsStream("lang/german.properties"));
         LanguageManager.addLanguageFile("english", Main.class.getClassLoader().getResourceAsStream("lang/english.properties"));
+        LanguageManager.addLanguageFile("french", Main.class.getClassLoader().getResourceAsStream("lang/french.properties"));
 
         ILogger logger = new DefaultAsyncLogger();
         initLogger(logger);
