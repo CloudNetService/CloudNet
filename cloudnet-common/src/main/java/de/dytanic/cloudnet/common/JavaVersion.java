e97d5af3d9f2d6b0655df4cb95d000e0aabce8c6
Julian <42209193+juliarn@users.noreply.github.com>
2020-10-22 15:21:25 +0200

    Fix starting failure when using an unknown java version

--- cloudnet-common/src/main/java/de/dytanic/cloudnet/common/JavaVersion.java
+++ cloudnet-common/src/main/java/de/dytanic/cloudnet/common/JavaVersion.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum JavaVersion {
+    UNKNOWN(-1, -1D, "Unknown Java"),
     JAVA_8(8, 52D, "Java 8"),
     JAVA_9(9, 53D, "Java 9"),
     JAVA_10(10, 54D, "Java 10"),
     JAVA_11(11, 55D, "Java 11"),
     JAVA_12(12, 56D, "Java 12"),
     JAVA_13(13, 57D, "Java 13"),
-    JAVA_14(14, 58D, "Java 14");
+    JAVA_14(14, 58D, "Java 14"),
+    JAVA_15(15, 59D, "Java 15"),
+    JAVA_16(16, 60D, "Java 16"),
+    JAVA_17(17, 61D, "Java 17");
 
     private final int version;
     private final double versionId;
@@ -34,21 +38,25 @@ public enum JavaVersion {
         return this.name;
     }
 
+    public static JavaVersion getRuntimeVersion() {
+        double versionId = Double.parseDouble(System.getProperty("java.class.version"));
+        return fromVersionId(versionId).orElse(UNKNOWN);
+    }
+
+    public boolean isUnknown() {
+        return this == UNKNOWN;
+    }
+
     public boolean isSupported(JavaVersion minJavaVersion, JavaVersion maxJavaVersion) {
-        return this.versionId >= minJavaVersion.versionId && this.versionId <= maxJavaVersion.versionId;
+        return this.isUnknown() || this.versionId >= minJavaVersion.versionId && this.versionId <= maxJavaVersion.versionId;
     }
 
     public boolean isSupportedByMin(JavaVersion minRequiredJavaVersion) {
-        return this.versionId >= minRequiredJavaVersion.versionId;
+        return this.isUnknown() || this.versionId >= minRequiredJavaVersion.versionId;
     }
 
     public boolean isSupportedByMax(JavaVersion maxRequiredJavaVersion) {
-        return this.versionId <= maxRequiredJavaVersion.versionId;
-    }
-
-    public static JavaVersion getRuntimeVersion() {
-        double versionId = Double.parseDouble(System.getProperty("java.class.version"));
-        return fromVersionId(versionId).orElseThrow(() -> new IllegalStateException("Running on an unsupported java version " + versionId));
+        return this.isUnknown() || this.versionId <= maxRequiredJavaVersion.versionId;
     }
 
     public static Optional<JavaVersion> fromVersionId(double versionId) {
