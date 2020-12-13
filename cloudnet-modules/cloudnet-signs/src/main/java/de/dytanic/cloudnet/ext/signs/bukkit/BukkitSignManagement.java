3f80fa8bc810f147cb17f92dae2f5daaafad8254
derklaro <pasqual.koschmieder@gmail.com>
2020-12-13 11:31:18 +0100

    Prevent sync chunk generation and load (Closes #381)

--- cloudnet-modules/cloudnet-signs/src/main/java/de/dytanic/cloudnet/ext/signs/bukkit/BukkitSignManagement.java
+++ cloudnet-modules/cloudnet-signs/src/main/java/de/dytanic/cloudnet/ext/signs/bukkit/BukkitSignManagement.java
@@ -11,10 +11,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.material.MaterialData;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,11 +24,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Iterator;
 
-public final class BukkitSignManagement extends AbstractSignManagement {
+public class BukkitSignManagement extends AbstractSignManagement {
 
     private final BukkitCloudNetSignsPlugin plugin;
 
-    BukkitSignManagement(BukkitCloudNetSignsPlugin plugin) {
+    protected BukkitSignManagement(BukkitCloudNetSignsPlugin plugin) {
         super();
 
         this.plugin = plugin;
@@ -40,28 +42,27 @@ public final class BukkitSignManagement extends AbstractSignManagement {
      * @deprecated SignManagement should be accessed via the {@link de.dytanic.cloudnet.common.registry.IServicesRegistry}
      */
     @Deprecated
+    @ApiStatus.ScheduledForRemoval
     public static BukkitSignManagement getInstance() {
         return (BukkitSignManagement) CloudNetDriver.getInstance().getServicesRegistry().getFirstService(AbstractSignManagement.class);
     }
 
-
     @Override
     protected void updateSignNext(@NotNull Sign sign, @NotNull SignLayout signLayout, @Nullable ServiceInfoSnapshot serviceInfoSnapshot) {
         Bukkit.getScheduler().runTask(this.plugin, () -> {
-            Location location = this.toLocation(sign.getWorldPosition());
+            final Location location = this.toLocation(sign.getWorldPosition());
+            if (location != null && location.getWorld() != null) {
+                final int chunkX = (int) Math.floor(location.getX()) >> 4;
+                final int chunkZ = (int) Math.floor(location.getZ()) >> 4;
 
-            if (location != null) {
-
-                Block block = location.getBlock();
-
-                if (block.getState() instanceof org.bukkit.block.Sign) {
-                    org.bukkit.block.Sign bukkitSign = (org.bukkit.block.Sign) block.getState();
-
-                    this.updateSign(location, sign, bukkitSign, signLayout, serviceInfoSnapshot);
+                if (location.getWorld().isChunkLoaded(chunkX, chunkZ)) {
+                    final Block block = location.getBlock();
+                    if (block.getState() instanceof org.bukkit.block.Sign) {
+                        org.bukkit.block.Sign bukkitSign = (org.bukkit.block.Sign) block.getState();
+                        this.updateSign(location, sign, bukkitSign, signLayout, serviceInfoSnapshot);
+                    }
                 }
-
             }
-
         });
     }
 
@@ -91,16 +92,13 @@ public final class BukkitSignManagement extends AbstractSignManagement {
         Preconditions.checkNotNull(bukkitSign);
         Preconditions.checkNotNull(signLayout);
 
-        if (signLayout.getLines() != null &&
-                signLayout.getLines().length == 4) {
-
+        if (signLayout.getLines() != null && signLayout.getLines().length == 4) {
             for (int i = 0; i < 4; i++) {
                 String line = ChatColor.translateAlternateColorCodes('&', super.replaceServiceInfo(signLayout.getLines()[i], sign.getTargetGroup(), serviceInfoSnapshot));
                 bukkitSign.setLine(i, line);
             }
 
             bukkitSign.update();
-
             this.changeBlock(location, signLayout.getBlockType(), signLayout.getSubId());
         }
     }
@@ -135,9 +133,7 @@ public final class BukkitSignManagement extends AbstractSignManagement {
                     }
                     backBlockState.update(true);
                 }
-
             }
-
         }
     }
 
@@ -150,20 +146,16 @@ public final class BukkitSignManagement extends AbstractSignManagement {
      */
     private BlockFace getSignFacing(BlockState blockState) {
         try {
-
             Method getBlockDataMethod = BlockState.class.getDeclaredMethod("getBlockData");
             Object blockData = getBlockDataMethod.invoke(blockState);
-
             Class<?> wallSignClass = Class.forName("org.bukkit.block.data.type.WallSign");
 
             if (wallSignClass.isInstance(blockData)) {
                 Method getFacingMethod = wallSignClass.getMethod("getFacing");
-
                 return (BlockFace) getFacingMethod.invoke(blockData);
             }
 
             return BlockFace.UP;
-
         } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException exception) {
             return null;
         }
@@ -172,17 +164,11 @@ public final class BukkitSignManagement extends AbstractSignManagement {
     public Location toLocation(WorldPosition worldPosition) {
         Preconditions.checkNotNull(worldPosition);
 
-        return Bukkit.getWorld(worldPosition.getWorld()) != null ? new Location(
-                Bukkit.getWorld(worldPosition.getWorld()),
-                worldPosition.getX(),
-                worldPosition.getY(),
-                worldPosition.getZ()
-        ) : null;
+        final World world = Bukkit.getWorld(worldPosition.getWorld());
+        return world == null ? null : new Location(world, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ());
     }
 
     public BukkitCloudNetSignsPlugin getPlugin() {
         return this.plugin;
     }
-
-
 }
