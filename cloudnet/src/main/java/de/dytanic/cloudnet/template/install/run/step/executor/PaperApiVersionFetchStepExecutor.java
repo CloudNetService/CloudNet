f7a7444f870d2107f2164a37c9d321e642aa1daf
derklaro <pasqual.koschmieder@gmail.com>
2021-01-05 20:51:45 +0100

    Fetch latest build using paper-api for papermc software

---
+++ cloudnet/src/main/java/de/dytanic/cloudnet/template/install/run/step/executor/PaperApiVersionFetchStepExecutor.java
@@ -0,0 +1,79 @@
+package de.dytanic.cloudnet.template.install.run.step.executor;
+
+import com.google.gson.reflect.TypeToken;
+import de.dytanic.cloudnet.CloudNet;
+import de.dytanic.cloudnet.common.document.gson.JsonDocument;
+import de.dytanic.cloudnet.template.install.ServiceVersionType;
+import de.dytanic.cloudnet.template.install.run.InstallInformation;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+public class PaperApiVersionFetchStepExecutor implements InstallStepExecutor {
+
+    private static final String DOWNLOAD_URL = "https://papermc.io/api/v2/projects/%s/versions/%s/builds/%d/downloads/%s-%s-%d.jar";
+    private static final Type INT_SET_TYPE = TypeToken.getParameterized(Set.class, Integer.class).getType();
+
+    @Override
+    public @NotNull Set<Path> execute(@NotNull InstallInformation installInformation, @NotNull Path workingDirectory, @NotNull Set<Path> inputPaths) throws IOException {
+        boolean enabled = installInformation.getServiceVersion().getProperties().getBoolean("fetch-over-paper-api");
+        String versionGroup = installInformation.getServiceVersion().getProperties().getString("version-group");
+        if (enabled && versionGroup != null) {
+            String project = this.decideApiProjectName(installInformation.getServiceVersionType());
+            JsonDocument versionInformation = this.makeRequest("https://papermc.io/api/v2/projects/" + project + "/versions/" + versionGroup);
+            if (versionInformation.contains("builds")) {
+                Set<Integer> builds = versionInformation.get("builds", INT_SET_TYPE);
+                Optional<Integer> newestBuild = builds.stream().reduce(Math::max);
+                if (newestBuild.isPresent()) {
+                    int build = newestBuild.get();
+                    installInformation.getServiceVersion().setUrl(String.format(DOWNLOAD_URL, project, versionGroup, build, project, versionGroup, build));
+                } else {
+                    CloudNet.getInstance().getLogger().error("Unable to retrieve latest build for papermc project " + project + " version-group " + versionGroup);
+                }
+            } else {
+                CloudNet.getInstance().getLogger().error("Unable to load build information for papermc project " + project + " version-group " + versionGroup);
+            }
+        }
+
+        return Collections.emptySet();
+    }
+
+    private JsonDocument makeRequest(@NotNull String apiUrl) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            connection.setRequestProperty("accepts", "application/json");
+            connection.connect();
+            if (connection.getResponseCode() == 200) {
+                try (InputStream stream = connection.getInputStream()) {
+                    return JsonDocument.newDocument(stream);
+                }
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return JsonDocument.EMPTY;
+    }
+
+    @NotNull
+    private String decideApiProjectName(@NotNull ServiceVersionType type) {
+        switch (type.getName().toLowerCase()) {
+            case "paperspigot":
+                return "paper";
+            case "waterfall":
+                return "waterfall";
+            case "travertine":
+                return "travertine";
+            default:
+                throw new IllegalStateException("Unsupported paper api project type " + type.getName());
+        }
+    }
+}
