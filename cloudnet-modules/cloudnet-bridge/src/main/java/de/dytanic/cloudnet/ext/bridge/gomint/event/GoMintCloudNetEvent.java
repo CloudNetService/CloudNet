3ddb784113aba8b8b25415cff983d7ffe10e6fbf
Julian <42209193+juliarn@users.noreply.github.com>
2021-01-17 16:42:03 +0100

    Fix GoMint javadoc

--- cloudnet-modules/cloudnet-bridge/src/main/java/de/dytanic/cloudnet/ext/bridge/gomint/event/GoMintCloudNetEvent.java
+++ cloudnet-modules/cloudnet-bridge/src/main/java/de/dytanic/cloudnet/ext/bridge/gomint/event/GoMintCloudNetEvent.java
@@ -7,7 +7,7 @@ import io.gomint.event.Event;
 
 /**
  * All CloudNet events will mostly fire asynchronously, depending on how they were triggered.
- * Check {@link GoMint#isMainThread()} and treat the event appropriately.
+ * Check {@link GoMint#mainThread()} ()} and treat the event appropriately.
  */
 abstract class GoMintCloudNetEvent extends Event {
 
