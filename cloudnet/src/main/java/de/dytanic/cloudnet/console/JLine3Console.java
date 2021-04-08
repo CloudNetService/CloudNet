d002b0ff75a89e0a09a8ebb0359965f6fab97361
0utplay <aldin@sijamhodzic.de>
2021-04-08 23:14:31 +0200

    Catch throwable instead of UnsatisfiedLinkError

--- cloudnet/src/main/java/de/dytanic/cloudnet/console/JLine3Console.java
+++ cloudnet/src/main/java/de/dytanic/cloudnet/console/JLine3Console.java
@@ -50,7 +50,7 @@ public final class JLine3Console implements IConsole {
 
         try {
             AnsiConsole.systemInstall();
-        } catch (UnsatisfiedLinkError ignored) {
+        } catch (Throwable ignored) {
         }
 
         this.terminal = TerminalBuilder.builder().system(true).encoding(StandardCharsets.UTF_8).build();
