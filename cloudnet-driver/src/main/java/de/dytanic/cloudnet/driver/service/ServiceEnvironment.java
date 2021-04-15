b76c671328ee42c788a8888dae5ab8fed94470a5
derklaro <pasqual.koschmieder@gmail.com>
2021-04-15 20:02:30 +0200

    Fix not shown typo 'tunity' to 'tuinity'

--- cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/service/ServiceEnvironment.java
+++ cloudnet-driver/src/main/java/de/dytanic/cloudnet/driver/service/ServiceEnvironment.java
@@ -8,7 +8,7 @@ public enum ServiceEnvironment {
     MINECRAFT_SERVER_DEFAULT("minecraft"),
     MINECRAFT_SERVER_SPIGOT("spigot"),
     MINECRAFT_SERVER_PAPER_SPIGOT("paper"),
-    MINECRAFT_SERVER_TUINITY_SPIGOT("tunity"),
+    MINECRAFT_SERVER_TUINITY_SPIGOT("tuinity"),
     MINECRAFT_SERVER_FORGE("forge"),
     MINECRAFT_SERVER_SPONGE_VANILLA("spongevanilla"),
     MINECRAFT_SERVER_AKARIN("akarin"),
