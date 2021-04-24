2e05c75e383b0f1593e848e978f9826088b190cb
Lvkas_ <51173477+zLvkas@users.noreply.github.com>
2021-04-24 15:40:59 +0200

    Add support for labymod's client permissions (#447)
    The labymod module now supports the permission system of the LabyMod client. Servers can enable and disable specific functions.

---
+++ cloudnet-modules/cloudnet-labymod/src/main/java/eu/cloudnetservice/cloudnet/ext/labymod/config/LabyModPermissionConfig.java
@@ -0,0 +1,31 @@
+package eu.cloudnetservice.cloudnet.ext.labymod.config;
+
+import java.util.Map;
+
+public class LabyModPermissionConfig {
+
+    private boolean enabled;
+    private Map<String, Boolean> labyModPermissions;
+
+    public LabyModPermissionConfig(boolean enabled, Map<String, Boolean> labyModPermissions) {
+        this.enabled = enabled;
+        this.labyModPermissions = labyModPermissions;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Map<String, Boolean> getLabyModPermissions() {
+        return this.labyModPermissions;
+    }
+
+    public void setLabyModPermissions(Map<String, Boolean> labyModPermissions) {
+        this.labyModPermissions = labyModPermissions;
+    }
+
+}
