c94d38cd908f0d07c810b2f01acfcde25ed3c955
derklaro <pasqual.koschmieder@gmail.com>
2021-05-28 07:41:55 +0200

    Convert the signs database if necessary

--- cloudnet-modules/cloudnet-signs/src/main/java/eu/cloudnetservice/cloudnet/ext/signs/node/CloudNetSignsModule.java
+++ cloudnet-modules/cloudnet-signs/src/main/java/eu/cloudnetservice/cloudnet/ext/signs/node/CloudNetSignsModule.java
@@ -1,16 +1,22 @@
 package eu.cloudnetservice.cloudnet.ext.signs.node;
 
+import de.dytanic.cloudnet.common.document.gson.JsonDocument;
 import de.dytanic.cloudnet.driver.database.Database;
 import de.dytanic.cloudnet.driver.module.ModuleLifeCycle;
 import de.dytanic.cloudnet.driver.module.ModuleTask;
+import de.dytanic.cloudnet.driver.util.DefaultModuleHelper;
+import de.dytanic.cloudnet.ext.bridge.WorldPosition;
+import de.dytanic.cloudnet.ext.signs.SignConstants;
 import de.dytanic.cloudnet.module.NodeCloudNetModule;
 import eu.cloudnetservice.cloudnet.ext.signs.GlobalChannelMessageListener;
+import eu.cloudnetservice.cloudnet.ext.signs.Sign;
 import eu.cloudnetservice.cloudnet.ext.signs.SignManagement;
 import eu.cloudnetservice.cloudnet.ext.signs.configuration.SignsConfiguration;
 import eu.cloudnetservice.cloudnet.ext.signs.node.commands.CommandSign;
 import eu.cloudnetservice.cloudnet.ext.signs.node.configuration.NodeSignsConfigurationHelper;
 
 import java.nio.file.Path;
+import java.util.Collection;
 
 public class CloudNetSignsModule extends NodeCloudNetModule {
 
@@ -41,9 +47,51 @@ public class CloudNetSignsModule extends NodeCloudNetModule {
                 new NodeSignsListener(management));
     }
 
+    @Deprecated
+    @ModuleTask(order = 20)
+    public void handleDatabaseConvert() {
+        this.convertDatabaseIfNecessary();
+    }
+
     @ModuleTask(order = 40, event = ModuleLifeCycle.STOPPED)
     public void handleStopping() throws Exception {
         this.database.close();
         this.getCloudNet().getEventManager().unregisterListeners(this.getClass().getClassLoader());
+    }
+
+    @Deprecated
+    private void convertDatabaseIfNecessary() {
+        // load old database document
+        Database database = this.getDatabaseProvider().getDatabase(DefaultModuleHelper.DEFAULT_CONFIGURATION_DATABASE_NAME);
+        JsonDocument document = database.get("signs_store");
+        // when the document is null the conversation already happened
+        if (document != null) {
+            // notify the user about the change
+            this.getCloudNet().getLogger().warning("Detected old signs database, running conversation...");
+            // remove the old document from the database
+            database.delete("signs_store");
+            // check if the old sign document even contains the signs
+            Collection<de.dytanic.cloudnet.ext.signs.Sign> oldSigns = document.get("signs", SignConstants.COLLECTION_SIGNS);
+            if (oldSigns != null) {
+                // convert the old sign entries
+                SignManagement management = this.getCloudNet().getServicesRegistry().getFirstService(SignManagement.class);
+                for (de.dytanic.cloudnet.ext.signs.Sign oldSign : oldSigns) {
+                    management.createSign(new Sign(
+                            oldSign.getTargetGroup(),
+                            oldSign.getProvidedGroup(),
+                            oldSign.getTemplatePath(),
+                            new WorldPosition(
+                                    oldSign.getWorldPosition().getX(),
+                                    oldSign.getWorldPosition().getY(),
+                                    oldSign.getWorldPosition().getZ(),
+                                    0,
+                                    0,
+                                    oldSign.getWorldPosition().getWorld(),
+                                    oldSign.getProvidedGroup()
+                            )
+                    ));
+                }
+            }
+        }
     }
 }
