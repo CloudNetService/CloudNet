aec64b46a477410e06f787a21c6d93346f0c8b90
a6doff <67640562+a6doff@users.noreply.github.com>
2021-06-13 14:11:49 +0200

    Update cloudnet-modules/cloudnet-report/src/main/java/de/dytanic/cloudnet/ext/report/CloudNetReportModule.java
    Co-authored-by: Pasqual Koschmieder <40468651+derklaro@users.noreply.github.com>

--- cloudnet-modules/cloudnet-report/src/main/java/de/dytanic/cloudnet/ext/report/CloudNetReportModule.java
+++ cloudnet-modules/cloudnet-report/src/main/java/de/dytanic/cloudnet/ext/report/CloudNetReportModule.java
@@ -95,7 +95,7 @@ public final class CloudNetReportModule extends NodeCloudNetModule {
 
 
   public void initSavingRecordsDirectory() {
-    if (this.getConfig().getBoolean("addCustomDate")) {
+    if (this.dateFormat != null && this.getConfig().getBoolean("addCustomDate")) {
       String date = this.dateFormat.format(System.currentTimeMillis());
       this.savingRecordsDirectory = this.getModuleWrapper().getDataDirectory()
         .resolve(this.getConfig().getString("recordDestinationDirectory") + "/" + date);
