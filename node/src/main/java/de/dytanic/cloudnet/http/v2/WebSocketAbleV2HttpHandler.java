91d78a3bfb3b074fd69ed0db0f55bbe85b3316be
derklaro <pasqual.koschmieder@gmail.com>
2021-10-16 21:17:13 +0200

    Fix semicolon position

--- node/src/main/java/de/dytanic/cloudnet/http/v2/WebSocketAbleV2HttpHandler.java
+++ node/src/main/java/de/dytanic/cloudnet/http/v2/WebSocketAbleV2HttpHandler.java
@@ -68,6 +68,5 @@ public abstract class WebSocketAbleV2HttpHandler extends V2HttpHandler {
     this.send403(context, "Authentication required");
   }
 
-  protected abstract void handleTicketAuthorizedRequest(String path, IHttpContext context, HttpSession session)
-      ;
+  protected abstract void handleTicketAuthorizedRequest(String path, IHttpContext context, HttpSession session);
 }
