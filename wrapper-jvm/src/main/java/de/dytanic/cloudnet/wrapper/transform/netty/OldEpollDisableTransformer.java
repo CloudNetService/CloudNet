d7d7ff514ffd2095821ce318fc4f38ac55a45423
derklaro <git@derklaro.dev>
2021-12-19 10:45:54 +0100

    chore(wrapper): cleanup the epoll disabling transformer

--- wrapper-jvm/src/main/java/de/dytanic/cloudnet/wrapper/transform/netty/OldEpollDisableTransformer.java
+++ wrapper-jvm/src/main/java/de/dytanic/cloudnet/wrapper/transform/netty/OldEpollDisableTransformer.java
@@ -21,6 +21,7 @@ import lombok.NonNull;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
@@ -31,63 +32,34 @@ public final class OldEpollDisableTransformer implements Transformer {
 
   @Override
   public void transform(@NonNull String classname, @NonNull ClassNode classNode) {
-    var usesOldEpollCreateMethod = false;
     // check if epoll should get disabled
     for (var method : classNode.methods) {
       if (method.name.equals("<clinit>")) {
         // old versions of netty are invoking a method called "epollCreate", new ones are invoking "newEpollCreate"
         for (var instruction : method.instructions) {
           if (instruction instanceof MethodInsnNode methodInsnNode && methodInsnNode.name.equals("epollCreate")) {
-            usesOldEpollCreateMethod = true;
+            var instructions = new InsnList();
+            // if the old epoll method is used then just set the unavailability cause
+            instructions.add(new TypeInsnNode(Opcodes.NEW, Type.getInternalName(UnsupportedOperationException.class)));
+            instructions.add(new InsnNode(Opcodes.DUP));
+            instructions.add(new LdcInsnNode("Netty 4.0.X is incompatible with Java 9+"));
+            instructions.add(new MethodInsnNode(
+              Opcodes.INVOKESPECIAL,
+              Type.getInternalName(UnsupportedOperationException.class),
+              "<init>",
+              "(Ljava/lang/String;)V",
+              false));
+            instructions.add(new FieldInsnNode(
+              Opcodes.PUTSTATIC,
+              // This prevents shadow from renaming io/netty to eu/cloudnetservice/io/netty
+              String.join("/", "io", "netty", "channel", "epoll", "Epoll"),
+              "UNAVAILABILITY_CAUSE",
+              Type.getDescriptor(Throwable.class)));
+            instructions.add(new InsnNode(Opcodes.RETURN));
+            // put it before the first instruction of the method
+            method.instructions.insert(instructions);
+            return;
           }
-        }
-      }
-    }
-
-    // if there is an old method call
-    if (usesOldEpollCreateMethod) {
-      for (var method : classNode.methods) {
-        // isAvailable? nope
-        if (method.name.equals("isAvailable")) {
-          method.instructions.insert(new InsnNode(Opcodes.IRETURN));
-          method.instructions.insert(new InsnNode(Opcodes.ICONST_0));
-          continue;
-        }
-
-        // ensureAvailability? sure do
-        if (method.name.equals("ensureAvailability")) {
-          var instructions = new InsnList();
-          // just rethrow the result of unavailabilityCause()
-          instructions.add(new MethodInsnNode(
-            Opcodes.INVOKESTATIC,
-            // This prevents shadow from renaming io/netty to eu/cloudnetservice/io/netty
-            String.format("%s/%s/%s", "io", "netty", "channel/epoll/Epoll"),
-            "unavailabilityCause",
-            "()Ljava/lang/Throwable;",
-            false));
-          instructions.add(new InsnNode(Opcodes.ATHROW));
-          instructions.add(new InsnNode(Opcodes.RETURN));
-          // add the instructions
-          method.instructions.insert(instructions);
-          continue;
-        }
-
-        // unavailabilityCause? just not available
-        if (method.name.equals("unavailabilityCause")) {
-          var instructions = new InsnList();
-          // init a new throwable with no real information
-          instructions.add(new TypeInsnNode(Opcodes.NEW, Type.getInternalName(UnsatisfiedLinkError.class)));
-          instructions.add(new InsnNode(Opcodes.DUP));
-          instructions.add(new LdcInsnNode("failed to load the required native library"));
-          instructions.add(new MethodInsnNode(
-            Opcodes.INVOKESPECIAL,
-            Type.getInternalName(UnsatisfiedLinkError.class),
-            "<init>",
-            "(Ljava/lang/String;)V",
-            false));
-          instructions.add(new InsnNode(Opcodes.ARETURN));
-          // add the instructions
-          method.instructions.insert(instructions);
         }
       }
     }
