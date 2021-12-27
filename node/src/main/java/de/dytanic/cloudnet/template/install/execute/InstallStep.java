52cf409b26ade4f642fd85b4e1ef801add3d399d
derklaro <git@derklaro.dev>
2021-12-27 09:37:01 +0100

    chore: drop magma downloads again

--- node/src/main/java/de/dytanic/cloudnet/template/install/execute/InstallStep.java
+++ node/src/main/java/de/dytanic/cloudnet/template/install/execute/InstallStep.java
@@ -21,7 +21,6 @@ import de.dytanic.cloudnet.template.install.execute.defaults.BuildStepExecutor;
 import de.dytanic.cloudnet.template.install.execute.defaults.CopyFilterStepExecutor;
 import de.dytanic.cloudnet.template.install.execute.defaults.DeployStepExecutor;
 import de.dytanic.cloudnet.template.install.execute.defaults.DownloadStepExecutor;
-import de.dytanic.cloudnet.template.install.execute.defaults.MagmaInstallerExecutor;
 import de.dytanic.cloudnet.template.install.execute.defaults.PaperApiVersionFetchStepExecutor;
 import de.dytanic.cloudnet.template.install.execute.defaults.UnzipStepExecutor;
 import java.io.IOException;
@@ -36,8 +35,7 @@ public enum InstallStep {
   UNZIP(new UnzipStepExecutor()),
   COPY_FILTER(new CopyFilterStepExecutor()),
   DEPLOY(new DeployStepExecutor()),
-  PAPER_API(new PaperApiVersionFetchStepExecutor()),
-  MAGMA_BUILD(new MagmaInstallerExecutor());
+  PAPER_API(new PaperApiVersionFetchStepExecutor());
 
   private final InstallStepExecutor executor;
 
