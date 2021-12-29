65f4252235492d263d443249c503639434aaaabe
0utplay <aldin@sijamhodzic.de>
2021-12-29 19:15:16 +0100

    feat(node): Removed the default jvm flags from the config.json of the node
    Aikar flags are not really optimized for proxies. That's why we now removed them from the node's config and just per default add them to the "Global-Server" group.

--- node/src/main/java/de/dytanic/cloudnet/config/Configuration.java
+++ node/src/main/java/de/dytanic/cloudnet/config/Configuration.java
@@ -21,10 +21,7 @@ import de.dytanic.cloudnet.driver.network.HostAndPort;
 import de.dytanic.cloudnet.driver.network.cluster.NetworkCluster;
 import de.dytanic.cloudnet.driver.network.cluster.NetworkClusterNode;
 import de.dytanic.cloudnet.driver.network.ssl.SSLConfiguration;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import lombok.NonNull;
 
 public interface Configuration {
@@ -96,10 +93,6 @@ public interface Configuration {
 
   void runBlockedServiceStartTryLaterAutomatic(boolean runBlockedServiceStartTryLaterAutomatic);
 
-  @NonNull DefaultJVMFlags defaultJVMFlags();
-
-  void defaultJVMFlags(@NonNull DefaultJVMFlags defaultJVMFlags);
-
   @NonNull String javaCommand();
 
   void javaCommand(@NonNull String javaCommand);
@@ -115,51 +108,5 @@ public interface Configuration {
   @NonNull JsonDocument properties();
 
   void properties(@NonNull JsonDocument properties);
-
-  enum DefaultJVMFlags {
-    NONE(Collections.emptyList()),
-    DYTANIC(Arrays.asList(
-      "-XX:+UseG1GC",
-      "-XX:MaxGCPauseMillis=50",
-      "-XX:-UseAdaptiveSizePolicy",
-      "-XX:CompileThreshold=100",
-      "-XX:+UnlockExperimentalVMOptions",
-      "-XX:+UseCompressedOops"
-    )),
-    // https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/
-    AIKAR(Arrays.asList(
-      "-XX:+UseG1GC",
-      "-XX:+ParallelRefProcEnabled",
-      "-XX:MaxGCPauseMillis=200",
-      "-XX:+UnlockExperimentalVMOptions",
-      "-XX:+DisableExplicitGC",
-      "-XX:+AlwaysPreTouch",
-      "-XX:G1NewSizePercent=30",
-      "-XX:G1MaxNewSizePercent=40",
-      "-XX:G1HeapRegionSize=8M",
-      "-XX:G1ReservePercent=20",
-      "-XX:G1HeapWastePercent=5",
-      "-XX:G1MixedGCCountTarget=4",
-      "-XX:InitiatingHeapOccupancyPercent=15",
-      "-XX:G1MixedGCLiveThresholdPercent=90",
-      "-XX:G1RSetUpdatingPauseTimePercent=5",
-      "-XX:SurvivorRatio=32",
-      "-XX:+PerfDisableSharedMem",
-      "-XX:MaxTenuringThreshold=1",
-      "-Dusing.aikars.flags=https://mcflags.emc.gs",
-      "-Daikars.new.flags=true"
-    ));
-
-    private final List<String> jvmFlags;
-
-    DefaultJVMFlags(List<String> jvmFlags) {
-      this.jvmFlags = jvmFlags;
-    }
-
-    public List<String> jvmFlags() {
-      return this.jvmFlags;
-    }
-
-  }
 
 }
