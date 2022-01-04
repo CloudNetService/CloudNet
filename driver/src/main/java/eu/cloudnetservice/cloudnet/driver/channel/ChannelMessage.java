64ce0755ef5f7ff6a991d213cb32b9e711614b7f
derklaro <git@derklaro.dev>
2022-01-04 09:09:19 +0100

    docs(driver): document all channel message classes

--- driver/src/main/java/eu/cloudnetservice/cloudnet/driver/channel/ChannelMessage.java
+++ driver/src/main/java/eu/cloudnetservice/cloudnet/driver/channel/ChannelMessage.java
@@ -17,9 +17,11 @@
 package eu.cloudnetservice.cloudnet.driver.channel;
 
 import com.google.common.base.Verify;
+import com.google.common.base.VerifyException;
 import eu.cloudnetservice.cloudnet.common.concurrent.Task;
 import eu.cloudnetservice.cloudnet.driver.CloudNetDriver;
 import eu.cloudnetservice.cloudnet.driver.DriverEnvironment;
+import eu.cloudnetservice.cloudnet.driver.channel.ChannelMessageTarget.Type;
 import eu.cloudnetservice.cloudnet.driver.network.buffer.DataBuf;
 import eu.cloudnetservice.cloudnet.driver.provider.CloudMessenger;
 import eu.cloudnetservice.cloudnet.driver.service.ServiceEnvironmentType;
@@ -29,6 +31,42 @@ import lombok.NonNull;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a message object which can be sent over the network with specific targets in mind. Unlike direct packet
+ * communication, channel messages are not bound to a specific messaging channels but can rather get sent to all
+ * components which are somewhere connected in the network. This means that it is possible to send a channel message to
+ * a service which is running on another node than the current service which is sending the channel message.
+ * <p>
+ * A channel message has two main identification points. One is the {@code channel} to which the message gets sent. The
+ * channel is a string object which is generally used to collect multiple types of channel message to a collection of
+ * message types. On the other hand a channel message contains a {@code message} object which should be unique within in
+ * the network and is used to identify a channel message in a group messages sent to the same {@code channel}.
+ * <p>
+ * The message contains a {@link DataBuf} containing the actual content of the message. There is no real way to identify
+ * which types are in the buffer or not, therefore it is crucial that a channel message gets identified via its {@code
+ * channel} and/or {@code message}.
+ * <p>
+ * If targets were given that are not locatable in the network they will get ignored silently. On the other hand this
+ * means that if you try to send a channel message to a non-existing target, the send method will block until the future
+ * wait timeout (30 seconds by default) expired before returning.
+ * <p>
+ * Note: there is no guarantee that the sender of a channel message is the actual component sending the message, as the
+ * message can be modified on its way to the receiver.
+ * <p>
+ * The actual constructor of this class shouldn't get used. Use {@link #builder()} instead.
+ *
+ * @param channel The channel to which the channel message gets sent. Mostly for identification reasons.
+ * @param message The message key of this channel message. Mostly for identification reasons.
+ * @param content The content of this channel message (the actual data to send).
+ * @param sender  The sender of the channel message. Should be, but must not the current network component.
+ * @param targets The targets to which the channel message should get send.
+ * @author Pasqual Koschmieder (derklaro@cloudnetservice.eu)
+ * @author Aldin (0utplay@cloudnetservice.eu)
+ * @see ChannelMessageSender
+ * @see ChannelMessageTarget
+ * @see eu.cloudnetservice.cloudnet.driver.event.events.channel.ChannelMessageReceiveEvent
+ * @since 4.0
+ */
 public record ChannelMessage(
   @NonNull String channel,
   @NonNull String message,
@@ -37,40 +75,111 @@ public record ChannelMessage(
   @NonNull Collection<ChannelMessageTarget> targets
 ) {
 
+  /**
+   * Constructs a new, empty builder for a ChannelMessage.
+   *
+   * @return a new, empty builder for a ChannelMessage.
+   */
   @Contract(" -> new")
   public static @NonNull Builder builder() {
     return new Builder();
   }
 
+  /**
+   * Constructs a new builder which contains all needed data to respond to a channel message. As the channel message
+   * will get directly handled by the waiting future, there is no need to actually set the channel and message of the
+   * returned builder. The new builder will target the sender of the given {@code input} and has no data set.
+   *
+   * @param input the channel message to respond to.
+   * @return a new builder for a channel message holding all base information to respond to the given source message.
+   */
   @Contract("_ -> new")
   public static @NonNull Builder buildResponseFor(@NonNull ChannelMessage input) {
     return builder().channel("").message("").target(input.sender.type(), input.sender.name());
   }
 
+  /**
+   * Sends this channel message using the current messenger of the environment. This is in fact just a shortcut method
+   * for {@link CloudMessenger#sendChannelMessage(ChannelMessage)}. This method will not wait for the target component
+   * to respond (it doesn't event expects a response) but for the handling component to send the message.
+   *
+   * @see CloudNetDriver#messenger()
+   */
   public void send() {
     this.messenger().sendChannelMessage(this);
   }
 
+  /**
+   * Sends this channel message as a query and returns a future which waits for target component(s) to respond. This
+   * method is a shortcut for {@link CloudMessenger#sendChannelMessageQueryAsync(ChannelMessage)}. The future will be
+   * completed when the target component responds or the query future times out (after 30 seconds).
+   *
+   * @return a future completed with all responses of all target components of this channel message.
+   * @see CloudNetDriver#messenger()
+   */
   public @NonNull Task<Collection<ChannelMessage>> sendQueryAsync() {
     return this.messenger().sendChannelMessageQueryAsync(this);
   }
 
+  /**
+   * Sends this channel message as a query and returns a future which waits for target component(s) to respond. Only the
+   * first response of any target will get sent back to this component. This is in particular useful if there is only
+   * one target, or you are only expecting one component of the target components to respond. This is in fact just a
+   * shortcut method for {@link CloudMessenger#sendSingleChannelMessageQueryAsync(ChannelMessage)}. The future will be
+   * completed when one target component responds or the query future times out (after 30 seconds).
+   *
+   * @return a future completed with the first response of any target of this channel message.
+   * @see CloudNetDriver#messenger()
+   */
   public @NonNull Task<ChannelMessage> sendSingleQueryAsync() {
     return this.messenger().sendSingleChannelMessageQueryAsync(this);
   }
 
+  /**
+   * Sends this channel message as a query and suspends the calling thread until all responses is available or the query
+   * timeout of 30 seconds is exceeded. This method is a shortcut for {@link CloudMessenger#sendChannelMessageQuery(ChannelMessage)}.
+   *
+   * @return all responses of all components this channel message is targeting.
+   * @see CloudNetDriver#messenger()
+   */
   public @NonNull Collection<ChannelMessage> sendQuery() {
     return this.messenger().sendChannelMessageQuery(this);
   }
 
+  /**
+   * Sends this channel message as a query and returns and blocks until one of the target component responded to this
+   * message or the query timeout of 30 seconds is exceeded. This is in particular useful if there is only one target,
+   * or you are only expecting one component of the target components to respond. This is in fact just a shortcut method
+   * for {@link CloudMessenger#sendSingleChannelMessageQueryAsync(ChannelMessage)}.
+   *
+   * @return the first response of any component this message is targeting.
+   * @see CloudNetDriver#messenger()
+   */
   public @Nullable ChannelMessage sendSingleQuery() {
     return this.messenger().sendSingleChannelMessageQuery(this);
   }
 
+  /**
+   * Util method to get the current messenger of the environment.
+   *
+   * @return the current messenger of the environment.
+   * @see CloudNetDriver#messenger()
+   */
   private @NonNull CloudMessenger messenger() {
     return CloudNetDriver.instance().messenger();
   }
 
+  /**
+   * A builder for a channel message. This class should be used over direct constructor access as allows better
+   * customization and validation of a channel message which gets created. Required properties are:
+   * <ul>
+   *   <li>channel, can be empty
+   *   <li>message, can be empty
+   *   <li>at least one target for the message
+   * </ul>
+   * <p>
+   * If no sender of the message is given the current network component will be used as the sender of the message.
+   */
   public static final class Builder {
 
     private final Collection<ChannelMessageTarget> targets = new ArrayList<>();
@@ -81,73 +190,181 @@ public record ChannelMessage(
     private DataBuf content;
     private ChannelMessageSender sender;
 
+    /**
+     * Sets the sender of this message. If no sender is given the current component will be used as the sender. Note
+     * that if you change the sender and try to receive a query response it will send the response to the given sender,
+     * not this component.
+     *
+     * @param sender the sender of this message.
+     * @return the same builder as used to call the method, for chaining.
+     * @see ChannelMessageSender#self()
+     */
     public @NonNull Builder sender(@NonNull ChannelMessageSender sender) {
       this.sender = sender;
       return this;
     }
 
+    /**
+     * Sets the channel of this message. Might be empty but should be unique to identify within the network.
+     *
+     * @param channel the channel.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder channel(@NonNull String channel) {
       this.channel = channel;
       return this;
     }
 
+    /**
+     * Sets the message key of this message. Might be empty but should be unique to identify within the network.
+     *
+     * @param message the message key.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder message(@NonNull String message) {
       this.message = message;
       return this;
     }
 
+    /**
+     * Sets the content of this message. If no content was given an empty buffer will be used.
+     *
+     * @param dataBuf the content.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder buffer(@Nullable DataBuf dataBuf) {
       this.content = dataBuf;
       return this;
     }
 
+    /**
+     * Adds the given target as a target of this message.
+     *
+     * @param target the target to add.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder target(@NonNull ChannelMessageTarget target) {
       this.targets.add(target);
       return this;
     }
 
+    /**
+     * Adds a channel message target to this message. You may not target an environment using this method.
+     *
+     * @param type the type of the receiver.
+     * @param name the name of the receiver.
+     * @return the same builder as used to call the method, for chaining.
+     * @throws IllegalArgumentException if type is {@link Type#ENVIRONMENT}
+     * @see ChannelMessageTarget#of(Type, String)
+     */
     public @NonNull Builder target(@NonNull ChannelMessageTarget.Type type, @Nullable String name) {
       return this.target(ChannelMessageTarget.of(type, name));
     }
 
+    /**
+     * Adds a channel message target to this message. You may not target an environment using this method.
+     *
+     * @param environment the driver environment to target.
+     * @param name        the name of the target, might be null to target all components with the given environment.
+     * @return the same builder as used to call the method, for chaining.
+     * @throws IllegalArgumentException if type is {@link Type#ENVIRONMENT}
+     * @see ChannelMessageTarget#of(Type, String)
+     */
     public @NonNull Builder target(@NonNull DriverEnvironment environment, @Nullable String name) {
       return this.target(environment == DriverEnvironment.CLOUDNET
         ? ChannelMessageTarget.Type.NODE
         : ChannelMessageTarget.Type.SERVICE, name);
     }
 
+    /**
+     * Targets all components with the given type. You may not target an environment using this method.
+     *
+     * @param type the type of the receivers to target.
+     * @return the same builder as used to call the method, for chaining.
+     * @throws IllegalArgumentException if type is {@link Type#ENVIRONMENT}
+     * @see ChannelMessageTarget#of(Type, String)
+     */
     public @NonNull Builder targetAll(@NonNull ChannelMessageTarget.Type type) {
       return this.target(type, null);
     }
 
+    /**
+     * Targets all components within the network.
+     *
+     * @return the same builder as used to call the method, for chaining.
+     * @throws IllegalArgumentException if type is {@link Type#ENVIRONMENT}
+     * @see ChannelMessageTarget#of(Type, String)
+     */
     public @NonNull Builder targetAll() {
       return this.target(ChannelMessageTarget.Type.ALL, null);
     }
 
+    /**
+     * Targets all services within the network.
+     *
+     * @return the same builder as used to call the method, for chaining.
+     * @throws IllegalArgumentException if type is {@link Type#ENVIRONMENT}
+     * @see ChannelMessageTarget#of(Type, String)
+     */
     public @NonNull Builder targetServices() {
       return this.targetAll(ChannelMessageTarget.Type.SERVICE);
     }
 
+    /**
+     * Targets a specific service in the network.
+     *
+     * @param name the name of the service to target.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder targetService(@Nullable String name) {
       return this.target(ChannelMessageTarget.Type.SERVICE, name);
     }
 
+    /**
+     * Targets all services of the given task within the network.
+     *
+     * @param name the name of the task to target.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder targetTask(@Nullable String name) {
       return this.target(ChannelMessageTarget.Type.TASK, name);
     }
 
+    /**
+     * Targets a specific node within the network.
+     *
+     * @param name the name of the node to target.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder targetNode(@Nullable String name) {
       return this.target(ChannelMessageTarget.Type.NODE, name);
     }
 
+    /**
+     * Targets all nodes within the network.
+     *
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder targetNodes() {
       return this.targetAll(ChannelMessageTarget.Type.NODE);
     }
 
+    /**
+     * Targets all services with the given environment within the network.
+     *
+     * @param environment the environment to target.
+     * @return the same builder as used to call the method, for chaining.
+     */
     public @NonNull Builder targetEnvironment(@NonNull ServiceEnvironmentType environment) {
       return this.target(ChannelMessageTarget.environment(environment));
     }
 
+    /**
+     * Builds a channel message from this builder within the contract given in the Builder class java docs.
+     *
+     * @return the created channel message from this builder.
+     * @throws VerifyException if message or channel is null or no target was specified.
+     */
     @Contract(" -> new")
     public @NonNull ChannelMessage build() {
       Verify.verifyNotNull(this.channel, "No channel provided");
--- driver/src/main/java/eu/cloudnetservice/cloudnet/driver/channel/ChannelMessageTarget.java
+++ driver/src/main/java/eu/cloudnetservice/cloudnet/driver/channel/ChannelMessageTarget.java
@@ -21,10 +21,36 @@ import eu.cloudnetservice.cloudnet.driver.service.ServiceEnvironmentType;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
-import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 
+/**
+ * Represents a receiver of a channel message. A channel message can be sent to a variety of receiver types including:
+ * <ul>
+ *   <li>ALL: includes all network components which are within the network
+ *   <li>NODE: includes all nodes which are within the network
+ *   <li>SERVICE: includes all services which are within the network
+ *   <li>TASK: includes all services of the given task
+ *   <li>GROUP: includes all services which are in the given group
+ *   <li>ENVIRONMENT: includes all services which are using the given environment. See {@link #environment(ServiceEnvironmentType)}
+ * </ul>
+ * <p>
+ * A channel message target can optionally take a name of the target. If no name is provided its interpreted as
+ * &quot;all components of the given type&quot;. It is illegal to create a target with no name given for the types
+ * {@link Type#TASK} and {@link Type#GROUP}. However, a creation of these types in combination with {@code null} as the
+ * name is a valid operation and will not fail.
+ * <p>
+ * Note: Types are not overriding meaning that setting a target for all services and (for example) specifically the
+ * service Abc-1 is valid and will cause the service to receive the channel message twice.
+ * <p>
+ * Do not extend this class to get access to it's constructors, use {@link #of(Type, String)} or for targeting a specific
+ * environment {@link #environment(ServiceEnvironmentType)} instead.
+ *
+ * @author Pasqual Koschmieder (derklaro@cloudnetservice.eu)
+ * @author Aldin (0utplay@cloudnetservice.eu)
+ * @see ChannelMessage
+ * @see ChannelMessageSender
+ */
 @ToString
 @EqualsAndHashCode
 public class ChannelMessageTarget {
@@ -37,29 +63,65 @@ public class ChannelMessageTarget {
   private final String name;
   private final ServiceEnvironmentType environment;
 
+  /**
+   * Constructs a new ChannelMessageTarget with no environment as its target.
+   *
+   * @param type the type of the target.
+   * @param name the optional name of the target.
+   * @throws NullPointerException if type is null.
+   */
   protected ChannelMessageTarget(@NonNull Type type, @Nullable String name) {
     this.type = type;
     this.name = name;
     this.environment = null;
   }
 
+  /**
+   * Constructs a new ChannelMessageTarget targeting a specific service environment. No name will be set.
+   *
+   * @param environment the environment of the target.
+   * @throws NullPointerException if environment is null.
+   */
   protected ChannelMessageTarget(@NonNull ServiceEnvironmentType environment) {
     this.type = Type.ENVIRONMENT;
     this.name = null;
     this.environment = environment;
   }
 
-  @Internal
-  public ChannelMessageTarget(Type type, String name, ServiceEnvironmentType environment) {
+  /**
+   * Internal method used for automated instantiation of this class.
+   *
+   * @param type        the type of the target.
+   * @param name        the optional name of the target.
+   * @param environment the environment of the target.
+   */
+  protected ChannelMessageTarget(Type type, String name, ServiceEnvironmentType environment) {
     this.type = type;
     this.name = name;
     this.environment = environment;
   }
 
+  /**
+   * Constructs a new ChannelMessageTarget targeting a specific service environment.
+   *
+   * @param type the environment to target.
+   * @return the created ChannelMessageTarget.
+   * @throws NullPointerException if type is null.
+   */
   public static @NonNull ChannelMessageTarget environment(@NonNull ServiceEnvironmentType type) {
     return new ChannelMessageTarget(type);
   }
 
+  /**
+   * Constructs a new ChannelMessageTarget of the given type with the optional name set.
+   *
+   * @param type the type of the new channel message target.
+   * @param name optional name of the component with the given type to target.
+   * @return the created ChannelMessageTarget.
+   * @throws NullPointerException     if type is null.
+   * @throws IllegalArgumentException if type is ENVIRONMENT. For that purpose use {@link #environment(ServiceEnvironmentType)}
+   *                                  instead.
+   */
   public static @NonNull ChannelMessageTarget of(@NonNull Type type, @Nullable String name) {
     Preconditions.checkArgument(type != Type.ENVIRONMENT, "Unable to target environment using name");
     // check if we have a constant value for the type
@@ -79,25 +141,65 @@ public class ChannelMessageTarget {
     return new ChannelMessageTarget(type, name);
   }
 
+  /**
+   * Returns the type of network component this ChannelMessageTarget targets. Can never be null.
+   *
+   * @return the type of network component this ChannelMessageTarget targets.
+   */
   public @NonNull Type type() {
     return this.type;
   }
 
+  /**
+   * Returns the name of the network component this ChannelMessageTarget targets. This may not be null if the target
+   * type is TASK or GROUP, but will always be null if the type is ENVIRONMENT. If a name is given when this channel
+   * message target has its type set to ALL it will silently be ignored.
+   *
+   * @return the name of the network component this ChannelMessageTarget targets.
+   */
   public @UnknownNullability String name() {
     return this.name;
   }
 
+  /**
+   * Returns the service environment this ChannelMessageTarget targets. This may not be null if the target type is
+   * ENVIRONMENT. In all other cases the environment can be non-null but will silently get ignored.
+   *
+   * @return the service environment this ChannelMessageTarget targets.
+   */
   public @UnknownNullability ServiceEnvironmentType environment() {
     return this.environment;
   }
 
+  /**
+   * Represents a type of network component this channel message targets.
+   */
   public enum Type {
 
+    /**
+     * Represents all network components in the network. A set name or environment will always get ignored.
+     */
     ALL,
+    /**
+     * Represents a node within the network. A name can optionally be present to target a specific node.
+     */
     NODE,
+    /**
+     * Represents a service within the network. A name can optionally be present to target a specific node.
+     */
     SERVICE,
+    /**
+     * Represents all services within the network which are using the given task as its base. A name must be present.
+     */
     TASK,
+    /**
+     * Represents all services within the network which are in the given group. A name must be present.
+     */
     GROUP,
+    /**
+     * Represents all services within the network which are using the given environment. An environment must be
+     * present.
+     */
     ENVIRONMENT
   }
 }
