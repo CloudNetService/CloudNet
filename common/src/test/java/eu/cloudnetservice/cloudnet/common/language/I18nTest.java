0a7f8c19efa2a7e7f7d440e1c5d3a00c8d6cf6f4
derklaro <git@derklaro.dev>
2022-01-18 11:14:45 +0100

    [ci skip] refactor(common): Make translation stuff easier to access for modules

--- common/src/test/java/eu/cloudnetservice/cloudnet/common/language/I18nTest.java
+++ common/src/test/java/eu/cloudnetservice/cloudnet/common/language/I18nTest.java
@@ -26,12 +26,14 @@ import org.junit.jupiter.api.Test;
 public class I18nTest {
 
   @Test
-  public void test() {
+  void testMessageRegistering() {
+    var loader = I18nTest.class.getClassLoader();
+
     var properties = new Properties();
     properties.put("test_message", "Test_Message");
 
     I18n.language("en");
-    I18n.addLanguageFile("en", properties);
+    I18n.addLanguageFile("en", properties, loader);
 
     Assertions.assertNotNull(I18n.trans("test_message"));
     Assertions.assertEquals("Test_Message", I18n.trans("test_message"));
@@ -41,19 +43,19 @@ public class I18nTest {
       properties.put(StringUtil.generateRandomString(5), StringUtil.generateRandomString(5));
     }
 
-    I18n.addLanguageFile("en", properties);
+    I18n.addLanguageFile("en", properties, loader);
     for (var entry : properties.entrySet()) {
       Assertions.assertEquals(entry.getValue(), I18n.trans(entry.getKey().toString()));
     }
   }
 
   @Test
-  public void testMessageFormatting() {
+  void testMessageFormatting() {
     var directory = Path.of("../node/src/main/resources/lang");
     // walk the lang directory and try to parse all translations to check if there are no unclosed formatting characters
     FileUtil.walkFileTree(
       directory,
-      (root, sub) -> I18n.addLanguageFile("test_TEST", sub),
+      (root, sub) -> I18n.addLanguageFile("test_TEST", sub, I18nTest.class.getClassLoader()),
       true);
   }
 }
