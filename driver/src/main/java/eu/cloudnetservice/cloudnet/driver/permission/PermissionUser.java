ffe7aae6c672d56b55314f2314d9dd98b4b196b3
0utplay <aldin@sijamhodzic.de>
2022-02-01 19:03:49 +0100

    docs(driver): more permission docs

--- driver/src/main/java/eu/cloudnetservice/cloudnet/driver/permission/PermissionUser.java
+++ driver/src/main/java/eu/cloudnetservice/cloudnet/driver/permission/PermissionUser.java
@@ -41,6 +41,21 @@ public class PermissionUser extends AbstractPermissible {
   private final String hashedPassword;
   private final Set<PermissionUserGroupInfo> groups;
 
+  /**
+   * Constructs a new permission group. The group is not saved or updated in any way before {@link
+   * PermissionManagement#addPermissionUser(PermissionUser)} is called.
+   *
+   * @param uniqueId         the unique id of the user.
+   * @param hashedPassword   the hashed password of the user.
+   * @param groups           all groups the user is in.
+   * @param name             the name of the user.
+   * @param potency          the potency of the user.
+   * @param createdTime      the creation timestamp.
+   * @param permissions      all permissions of the user.
+   * @param groupPermissions all group specific permissions of the user.
+   * @param properties       extra properties for the permission user.
+   * @throws NullPointerException if any of the given parameters except the hashed password is null.
+   */
   protected PermissionUser(
     @NonNull UUID uniqueId,
     @Nullable String hashedPassword,
@@ -58,10 +73,22 @@ public class PermissionUser extends AbstractPermissible {
     this.groups = groups;
   }
 
+  /**
+   * Creates a new permission user builder instance with all default values.
+   *
+   * @return the new builder instance.
+   */
   public static @NonNull Builder builder() {
     return new Builder();
   }
 
+  /**
+   * Creates a new permission user builder instance and copies all values from the given permission user.
+   *
+   * @param user the group to copy from.
+   * @return the new builder instance.
+   * @throws NullPointerException if the given user is null.
+   */
   public static @NonNull Builder builder(@NonNull PermissionUser user) {
     return builder()
       .name(user.name())
@@ -76,41 +103,105 @@ public class PermissionUser extends AbstractPermissible {
       .groupPermissions(user.groupPermissions());
   }
 
+  /**
+   * Checks if the given password matches the stored password. The input is hashed using sha 256 and encoded using
+   * base64 and then compared to the stored value.
+   *
+   * @param password the password to compare.
+   * @return true if the password matches, false otherwise.
+   */
   public boolean checkPassword(@Nullable String password) {
     return this.hashedPassword != null
       && password != null
       && this.hashedPassword.equals(Base64.getEncoder().encodeToString(EncryptTo.encryptToSHA256(password)));
   }
 
+  /**
+   * Gets the unique id of the permission user.
+   *
+   * @return the unique id of the user.
+   */
   public @NonNull UUID uniqueId() {
     return this.uniqueId;
   }
 
+  /**
+   * Gets all groups of the permission user.
+   *
+   * @return all groups of the user.
+   */
   public @NonNull Collection<PermissionUserGroupInfo> groups() {
     return this.groups;
   }
 
+  /**
+   * Gets the password of the user hashed with sha 256. If no password is set null is returned.
+   *
+   * @return the hashed password.
+   */
   public @Nullable String hashedPassword() {
     return this.hashedPassword;
   }
 
+  /**
+   * Gets the name of all groups the user is in.
+   *
+   * @return the name of all groups of the user.
+   */
   @Override
   public @NonNull Collection<String> groupNames() {
     return this.groups().stream().map(PermissionUserGroupInfo::group).collect(Collectors.toList());
   }
 
+  /**
+   * Searches for any of the users groups with the given name and returns it wrapped in an optional.
+   *
+   * @param group the group to search for.
+   * @return the found group wrapped in an optional. The content of the optional is null if no group was found.
+   * @throws NullPointerException if the given group is null.
+   */
   public @NonNull Optional<PermissionUserGroupInfo> findAssignedGroup(@NonNull String group) {
     return this.groups.stream().filter(info -> info.group().equalsIgnoreCase(group)).findFirst();
   }
 
+  /**
+   * Adds the given group without any time-out to this permission user.
+   * <p>
+   * Note: No update is done until the user is manually updated using {@link PermissionManagement#updateUser(PermissionUser)}.
+   *
+   * @param group the group to add to the user.
+   * @return the same user instance for chaining.
+   * @throws NullPointerException if the given group is null.
+   */
   public @NonNull PermissionUser addGroup(@NonNull String group) {
     return this.addGroup(group, 0L);
   }
 
+  /**
+   * Adds the given group with the given time-out to this permission user. The given time-out should be a unix timestamp
+   * in the future or any negative int if the group should not expire.
+   * <p>
+   * Note: No update is done until the user is manually updated using {@link PermissionManagement#updateUser(PermissionUser)}.
+   *
+   * @param group         the group to add to the user.
+   * @param timeOutMillis the timestamp for the group expiry.
+   * @return the same usr instance for chaining.
+   * @throws NullPointerException if the given group is null.
+   */
   public @NonNull PermissionUser addGroup(@NonNull String group, long timeOutMillis) {
     return this.addGroup(PermissionUserGroupInfo.builder().group(group).timeOutMillis(timeOutMillis).build());
   }
 
+  /**
+   * Adds the given group user info to this permission user. Other groups with the same name are replaced by the given
+   * group info.
+   * <p>
+   * Note: No update is done until the user is manually updated using {@link PermissionManagement#updateUser(PermissionUser)}.
+   *
+   * @param groupInfo the group info of the new group for the user.
+   * @return the same user instance for chaining.
+   * @throws NullPointerException if the given group is null.
+   */
   public @NonNull PermissionUser addGroup(@NonNull PermissionUserGroupInfo groupInfo) {
     var oldInfo = this.groups().stream()
       .filter(info -> info.group().equalsIgnoreCase(groupInfo.group()))
@@ -125,11 +216,26 @@ public class PermissionUser extends AbstractPermissible {
     return this;
   }
 
-  public @NonNull PermissionUser removeGroup(@NonNull String group) {
-    this.groups.removeIf(info -> info.group().equalsIgnoreCase(group));
-    return this;
+  /**
+   * Removes every group that has the given name.
+   * <p>
+   * Note: No update is done until the user is manually updated using {@link PermissionManagement#updateUser(PermissionUser)}.
+   *
+   * @param group the group to remove.
+   * @return true if any group was removed, false otherwise.
+   * @throws NullPointerException if the given group is null.
+   */
+  public boolean removeGroup(@NonNull String group) {
+    return this.groups.removeIf(info -> info.group().equalsIgnoreCase(group));
   }
 
+  /**
+   * Checks if the user is in a permission group that has the given name.
+   *
+   * @param group the group to check if the user is in.
+   * @return true if the user is in any group with the given name, false otherwise.
+   * @throws NullPointerException if the given group is null.
+   */
   public boolean inGroup(@NonNull String group) {
     return this.groups().stream().anyMatch(info -> info.group().equalsIgnoreCase(group));
   }
