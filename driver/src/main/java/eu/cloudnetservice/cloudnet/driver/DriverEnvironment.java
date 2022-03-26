fc30265a4d04025ca4728e2ae6d151d6a4cc954f
derklaro <git@derklaro.dev>
2022-03-26 10:35:31 +0100

    [ci skip] finish up

--- driver/src/main/java/eu/cloudnetservice/cloudnet/driver/DriverEnvironment.java
+++ driver/src/main/java/eu/cloudnetservice/cloudnet/driver/DriverEnvironment.java
@@ -16,22 +16,49 @@
 
 package eu.cloudnetservice.cloudnet.driver;
 
+import eu.cloudnetservice.cloudnet.common.Nameable;
 import eu.cloudnetservice.cloudnet.common.document.gson.JsonDocument;
 import eu.cloudnetservice.cloudnet.common.document.property.JsonDocPropertyHolder;
 import lombok.NonNull;
 
-public final class DriverEnvironment extends JsonDocPropertyHolder {
+/**
+ * Represents the current environment in which the driver is running. There are currently two default environments, the
+ * node and wrapper which are statically represented in this class.
+ * <p>
+ * A driver environment can hold additional information in its json properties, these are however never persisted
+ * anywhere.
+ *
+ * @since 4.0
+ */
+public final class DriverEnvironment extends JsonDocPropertyHolder implements Nameable {
 
+  /**
+   * The jvm-static representation of the node environment.
+   */
   public static final DriverEnvironment NODE = new DriverEnvironment("node", JsonDocument.newDocument());
+  /**
+   * The jvm-static representation of the wrapper environment.
+   */
   public static final DriverEnvironment WRAPPER = new DriverEnvironment("wrapper", JsonDocument.newDocument());
 
   private final String name;
 
-  public DriverEnvironment(@NonNull String name, @NonNull JsonDocument document) {
-    super(document);
+  /**
+   * Constructs a new driver environment instance.
+   *
+   * @param name       the name of the environment.
+   * @param properties the properties of the environment.
+   * @throws NullPointerException if the given name or properties are null.
+   */
+  public DriverEnvironment(@NonNull String name, @NonNull JsonDocument properties) {
+    super(properties);
     this.name = name;
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
   public @NonNull String name() {
     return this.name;
   }
