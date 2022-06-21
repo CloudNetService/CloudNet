8ee03373199100f209429d48cfb70b3cc4c1445d
derklaro <git@derklaro.dev>
2022-06-21 10:13:08 +0200

    [ci skip] fix: either use new or old constructor of ServerListPingEvent

--- cloudnet-modules/cloudnet-bridge/src/main/java/de/dytanic/cloudnet/ext/bridge/bukkit/BukkitCloudNetBridgePlugin.java
+++ cloudnet-modules/cloudnet-bridge/src/main/java/de/dytanic/cloudnet/ext/bridge/bukkit/BukkitCloudNetBridgePlugin.java
@@ -25,13 +25,22 @@ import de.dytanic.cloudnet.ext.bridge.listener.BridgeCustomChannelMessageListene
 import de.dytanic.cloudnet.ext.bridge.player.IPlayerManager;
 import de.dytanic.cloudnet.ext.bridge.server.BridgeServerHelper;
 import de.dytanic.cloudnet.wrapper.Wrapper;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.function.Supplier;
+import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 
 public final class BukkitCloudNetBridgePlugin extends JavaPlugin {
+
+  private Supplier<ServerListPingEvent> serverListPingEventConstructor;
 
   @Override
   public synchronized void onEnable() {
@@ -70,14 +79,12 @@ public final class BukkitCloudNetBridgePlugin extends JavaPlugin {
       boolean value = false;
 
       try {
-        ServerListPingEvent serverListPingEvent = new ServerListPingEvent(
-          new InetSocketAddress("127.0.0.1", 53345).getAddress(),
-          BridgeServerHelper.getMotd(),
-          Bukkit.getOnlinePlayers().size(),
-          BridgeServerHelper.getMaxPlayers()
-        );
-        Bukkit.getPluginManager().callEvent(serverListPingEvent);
+        ServerListPingEvent serverListPingEvent = this.constructServerListPingEvent();
+        if (serverListPingEvent == null) {
+          return;
+        }
 
+        Bukkit.getPluginManager().callEvent(serverListPingEvent);
         if (!serverListPingEvent.getMotd().equalsIgnoreCase(BridgeServerHelper.getMotd())) {
           hasToUpdate = true;
           BridgeServerHelper.setMotd(serverListPingEvent.getMotd());
@@ -105,5 +112,49 @@ public final class BukkitCloudNetBridgePlugin extends JavaPlugin {
         exception.printStackTrace();
       }
     }, 0, 10);
+  }
+
+  private @Nullable ServerListPingEvent constructServerListPingEvent() {
+    if (this.serverListPingEventConstructor == null) {
+      try {
+        // new method in 1.19 and above
+        MethodHandle constructor = MethodHandles.publicLookup().findConstructor(
+          ServerListPingEvent.class,
+          MethodType.methodType(void.class, InetAddress.class, String.class, boolean.class, int.class, int.class));
+        this.serverListPingEventConstructor = () -> {
+          try {
+            return (ServerListPingEvent) constructor.invokeExact(
+              new InetSocketAddress("127.0.0.1", 53345).getAddress(),
+              BridgeServerHelper.getMotd(),
+              false,
+              Bukkit.getOnlinePlayers().size(),
+              BridgeServerHelper.getMaxPlayers());
+          } catch (Throwable throwable) {
+            // wait what
+            this.getLogger().log(
+              Level.SEVERE,
+              "Unable to construct ServerListPingEvent using modern constructor; disabling event calling",
+              throwable);
+            this.serverListPingEventConstructor = () -> null;
+            return null;
+          }
+        };
+      } catch (IllegalAccessException exception) {
+        // wait what
+        this.serverListPingEventConstructor = () -> null;
+        this.getLogger().log(
+          Level.SEVERE,
+          "Unable to access modern constructor of ServerListPingEvent; disabling event calling",
+          exception);
+      } catch (NoSuchMethodException exception) {
+        // old method before 1.18
+        this.serverListPingEventConstructor = () -> new ServerListPingEvent(
+          new InetSocketAddress("127.0.0.1", 53345).getAddress(),
+          BridgeServerHelper.getMotd(),
+          Bukkit.getOnlinePlayers().size(),
+          BridgeServerHelper.getMaxPlayers());
+      }
+    }
+    return this.serverListPingEventConstructor.get();
   }
 }
