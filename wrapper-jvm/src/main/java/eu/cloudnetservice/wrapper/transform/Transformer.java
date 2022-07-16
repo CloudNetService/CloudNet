efdeee0e4001443d81b2a134902566b6331c4d60
Pasqual Koschmieder <git@derklaro.dev>
2022-07-16 19:09:08 +0200

    docs: improve wrapper documentation (#715)
    ### Motivation
    The wrapper documentation is very old and outdated and should be updated.
    
    ### Modification
    Updated the documentation of the wrapper api and made some very small code changes to enhance the overall api.
    
    ### Result
    Cleaner & better documented wrapper api.

--- wrapper-jvm/src/main/java/eu/cloudnetservice/wrapper/transform/Transformer.java
+++ wrapper-jvm/src/main/java/eu/cloudnetservice/wrapper/transform/Transformer.java
@@ -19,8 +19,26 @@ package eu.cloudnetservice.wrapper.transform;
 import lombok.NonNull;
 import org.objectweb.asm.tree.ClassNode;
 
+/**
+ * A transformer for classes which are being loaded during the runtime. A transformer which is registered to a
+ * transformer registry gets called BEFORE a class definition, making it able to change the class as required. Class
+ * re-transformations are not posted to transformers.
+ *
+ * @since 4.0
+ */
 @FunctionalInterface
 public interface Transformer {
 
+  /**
+   * Applies the transformation to the given class node. The passed in class node will get re-encoded and represents the
+   * class after transformation (and therefore will be the final defined class).
+   * <p>
+   * This transformer is only called once, after that this transformer will automatically get removed from the
+   * registered transformers (as the class is defined and transformers will never handle re-transformations).
+   *
+   * @param classname the fully qualified name of the transforming class.
+   * @param classNode the class node in its original form, before any transformation was applied to it.
+   * @throws NullPointerException if the given class name or class node is null.
+   */
   void transform(@NonNull String classname, @NonNull ClassNode classNode);
 }
