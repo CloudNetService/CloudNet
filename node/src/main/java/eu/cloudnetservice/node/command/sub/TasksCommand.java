64b1281126bfbbef7545caefa9a2a95873ca89c3
0utplay <aldin@sijamhodzic.de>
2022-07-19 21:12:46 +0200

    chore: allow setting the host address, runtime & javaCommand

--- node/src/main/java/eu/cloudnetservice/node/command/sub/TasksCommand.java
+++ node/src/main/java/eu/cloudnetservice/node/command/sub/TasksCommand.java
@@ -761,6 +761,48 @@ public final class TasksCommand {
     }
   }
 
+  @CommandMethod("tasks task <name> unset javaCommand")
+  public void unsetJavaCommand(
+    @NonNull CommandSource source,
+    @NonNull @Argument("name") Collection<ServiceTask> serviceTasks
+  ) {
+    for (var serviceTask : serviceTasks) {
+      this.updateTask(serviceTask, builder -> builder.javaCommand(null));
+      source.sendMessage(I18n.trans("command-tasks-set-property-success",
+        "javaCommand",
+        serviceTask.name(),
+        "null"));
+    }
+  }
+
+  @CommandMethod("tasks task <name> unset hostAddress")
+  public void unsetHostAddress(
+    @NonNull CommandSource source,
+    @NonNull @Argument("name") Collection<ServiceTask> serviceTasks
+  ) {
+    for (var serviceTask : serviceTasks) {
+      this.updateTask(serviceTask, builder -> builder.hostAddress(null));
+      source.sendMessage(I18n.trans("command-tasks-set-property-success",
+        "hostAddress",
+        serviceTask.name(),
+        "null"));
+    }
+  }
+
+  @CommandMethod("tasks task <name> unset runtime")
+  public void unsetRuntime(
+    @NonNull CommandSource source,
+    @NonNull @Argument("name") Collection<ServiceTask> serviceTasks
+  ) {
+    for (var serviceTask : serviceTasks) {
+      this.updateTask(serviceTask, builder -> builder.runtime(null));
+      source.sendMessage(I18n.trans("command-tasks-set-property-success",
+        "runtime",
+        serviceTask.name(),
+        "null"));
+    }
+  }
+
   private void updateTask(@NonNull ServiceTask task, @NonNull Consumer<ServiceTask.Builder> consumer) {
     consumer
       .andThen(result -> this.taskProvider().addServiceTask(result.build()))
