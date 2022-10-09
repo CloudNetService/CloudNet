f7f9c6dccdb8565a42cf5e2d6dcda65c0adc123d
derklaro <git@derklaro.dev>
2022-10-09 20:29:54 +0200

    chore: release version 4.0.0-RC3

--- build-extensions/src/main/kotlin/Versions.kt
+++ build-extensions/src/main/kotlin/Versions.kt
@@ -17,7 +17,7 @@
 object Versions {
 
   // internal versions
-  const val cloudNet = "4.0.0-RC3-SNAPSHOT"
+  const val cloudNet = "4.0.0-RC3"
   const val cloudNetCodeName = "Blizzard"
 
   // external tools
