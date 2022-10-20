e0a9efda719dbe8249567484b122248830aa9651
Aldin <aldin@sijamhodzic.de>
2022-10-20 21:56:03 +0200

    chore: improve config handling in the cloudnet chat plugin (#931)
    ### Motivation
    The default configs from the jar were not extracted at runtime on sponge
    and bukkit.
    
    ### Modification
    Made bukkit and sponge extract their configs.
    
    ### Result
    The default configs are extracted if no other config exists.

--- plugins/chat/src/main/java/eu/cloudnetservice/plugins/chat/SpongeChatPlugin.java
+++ plugins/chat/src/main/java/eu/cloudnetservice/plugins/chat/SpongeChatPlugin.java
@@ -17,20 +17,32 @@
 package eu.cloudnetservice.plugins.chat;
 
 import com.google.inject.Inject;
+import dev.derklaro.reflexion.MethodAccessor;
+import dev.derklaro.reflexion.Reflexion;
+import eu.cloudnetservice.common.io.FileUtil;
 import eu.cloudnetservice.common.log.LogManager;
 import eu.cloudnetservice.common.log.Logger;
 import eu.cloudnetservice.ext.component.ComponentFormats;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 import lombok.NonNull;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.spongepowered.api.Platform;
+import org.spongepowered.api.ResourceKey;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.lifecycle.ConstructPluginEvent;
 import org.spongepowered.api.event.message.PlayerChatEvent;
+import org.spongepowered.api.registry.RegistryKey;
+import org.spongepowered.api.registry.RegistryReference;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurationNode;
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
@@ -41,21 +53,46 @@ import org.spongepowered.plugin.builtin.jvm.Plugin;
 public class SpongeChatPlugin {
 
   private static final Logger LOGGER = LogManager.logger(SpongeChatPlugin.class);
+  private static final MethodAccessor<Method> EVENT_CHAT_TYPE_METHOD = Reflexion
+    .find("org.spongepowered.api.event.message.PlayerChatEvent$Submit")
+    .flatMap(reflexion -> reflexion.findMethod("chatType"))
+    .orElse(null);
+  private static final MethodAccessor<Method> CANCEL_CHAT_EVENT_METHOD = Reflexion
+    .find("org.spongepowered.api.event.message.PlayerChatEvent$Submit")
+    .flatMap(reflexion -> reflexion.findMethod("setCancelled", boolean.class))
+    .orElse(null);
 
   private final Path configFilePath;
+  private final int spongeAPIVersion;
   private volatile String chatFormat;
 
   @Inject
   public SpongeChatPlugin(@ConfigDir(sharedRoot = false) @NonNull Path configDirectory) {
     this.configFilePath = configDirectory.resolve("config.conf");
+    this.spongeAPIVersion = Sponge.pluginManager().plugin(Platform.API_ID)
+      .map(plugin -> plugin.metadata().version().getMajorVersion())
+      .orElse(0);
   }
 
   @Listener
   public void handle(@NonNull ConstructPluginEvent event) {
     ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader.builder()
+      .emitComments(false)
       .path(this.configFilePath)
       .build();
+
     try {
+      // check if we should copy the default config that's located inside the jar
+      if (Files.notExists(this.configFilePath)) {
+        // retrieve the file from the jar
+        event.plugin().openResource(URI.create("config.conf")).ifPresent(in -> {
+          try (in) {
+            FileUtil.copy(in, this.configFilePath);
+          } catch (IOException ignored) {
+          }
+        });
+      }
+
       // load the root node
       ConfigurationNode root = loader.load();
       var format = root.node("format");
@@ -72,20 +109,53 @@ public class SpongeChatPlugin {
     }
   }
 
-  @Listener
+  @Listener(order = Order.LAST)
+  public void handleAPI10(@NonNull PlayerChatEvent event, @First @NonNull ServerPlayer player) {
+    // check if we are on sponge api 10 and the event is caused by a chat message submission
+    if (EVENT_CHAT_TYPE_METHOD != null && event.getClass().getSimpleName().endsWith("Submit$Impl")) {
+      // get the chat type using reflexion
+      var chatTypeResult = EVENT_CHAT_TYPE_METHOD.<RegistryReference<?>>invoke(event)
+        .map(RegistryKey::location)
+        .map(ResourceKey::asString)
+        .getOrElse(null);
+      if (chatTypeResult != null && chatTypeResult.equals("minecraft:chat")) {
+        var format = ChatFormatter.buildFormat(
+          player.uniqueId(),
+          player.name(),
+          LegacyComponentSerializer.legacySection().serialize(player.displayName().get()),
+          this.chatFormat,
+          LegacyComponentSerializer.legacySection().serialize(event.message()),
+          player::hasPermission,
+          (colorChar, message) -> message.replace(colorChar, '§'));
+        // always cancel the event as we want to broadcast the message
+        if (CANCEL_CHAT_EVENT_METHOD != null) {
+          CANCEL_CHAT_EVENT_METHOD.invoke(event, true);
+        }
+
+        // broadcast the new message if the formatting was successful
+        if (format != null) {
+          Sponge.server().broadcastAudience().sendMessage(ComponentFormats.BUNGEE_TO_ADVENTURE.convert(format));
+        }
+      }
+    }
+  }
+
+  @Listener(order = Order.LAST)
   public void handle(@NonNull PlayerChatEvent event, @First @NonNull ServerPlayer player) {
-    var format = ChatFormatter.buildFormat(
-      player.uniqueId(),
-      player.name(),
-      LegacyComponentSerializer.legacySection().serialize(player.displayName().get()),
-      this.chatFormat,
-      LegacyComponentSerializer.legacySection().serialize(event.message()),
-      player::hasPermission,
-      (colorChar, message) -> message.replace(colorChar, '§'));
-    if (format == null) {
-      event.setCancelled(true);
-    } else {
-      event.setChatFormatter(($, $1, $2, $3) -> Optional.of(ComponentFormats.BUNGEE_TO_ADVENTURE.convert(format)));
+    if (this.spongeAPIVersion < 10) {
+      var format = ChatFormatter.buildFormat(
+        player.uniqueId(),
+        player.name(),
+        LegacyComponentSerializer.legacySection().serialize(player.displayName().get()),
+        this.chatFormat,
+        LegacyComponentSerializer.legacySection().serialize(event.message()),
+        player::hasPermission,
+        (colorChar, message) -> message.replace(colorChar, '§'));
+      if (format == null) {
+        event.setCancelled(true);
+      } else {
+        event.setChatFormatter(($, $1, $2, $3) -> Optional.of(ComponentFormats.BUNGEE_TO_ADVENTURE.convert(format)));
+      }
     }
   }
 }
