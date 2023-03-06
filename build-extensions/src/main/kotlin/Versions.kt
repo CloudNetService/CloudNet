90a5b0ace5fa79d1ecee6113b1619f3c5d175489
derklaro <git@derklaro.dev>
2023-03-06 19:43:53 +0100

    chore: release version 4.0.0-RC8

--- build-extensions/src/main/kotlin/Versions.kt
+++ build-extensions/src/main/kotlin/Versions.kt
@@ -17,7 +17,7 @@
 object Versions {
 
   // internal versions
-  const val cloudNet = "4.0.0-RC8-SNAPSHOT"
+  const val cloudNet = "4.0.0-RC8"
   const val cloudNetCodeName = "Blizzard"
 
   // external tools
