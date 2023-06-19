06489d39efe30168b59a4007fb5aa8715e68efba
0utplay <aldin@sijamhodzic.de>
2023-06-19 11:54:16 +0200

    chore: document the `@HandlerScope` annotation

--- node/src/main/java/eu/cloudnetservice/node/http/annotation/HandlerScope.java
+++ node/src/main/java/eu/cloudnetservice/node/http/annotation/HandlerScope.java
@@ -22,20 +22,30 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import lombok.NonNull;
+import org.jetbrains.annotations.ApiStatus;
 
 /**
- * Enforces a permission for the annotated http handler. If this annotation is present on class and method level, then
- * the annotation on method level is used.
+ * Enforces a scope for the annotated http handler. If this annotation is present on class and method level, then the
+ * annotation on method level is used.
  * <p>
  * This annotation must be used in combination with {@code @BasicAuth} or {@code @BearerAuth} to have any effect.
  *
  * @since 4.0
  */
 @Documented
+@ApiStatus.Experimental
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface HandlerScope {
 
-  // TODO
+  /**
+   * The scopes that are enforced on the http handler annotated with this annotation. The caller of this handler must
+   * have at least one of the given scopes in order to successfully call this handler.
+   * <p>
+   * Note: All supplied scopes have to follow the scope pattern described in
+   * {@link eu.cloudnetservice.node.http.RestUserManagement#SCOPE_NAMING_REGEX}.
+   *
+   * @return the scopes that are enforced on the http handler annotated with this annotation.
+   */
   @NonNull String[] value();
 }
