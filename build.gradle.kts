5616210fd1b60d8ad379f2859ffd62beabc2bf60
Aldin <aldin@sijamhodzic.de>
2024-03-06 13:43:41 +0100

    chore: update all dependencies (#1358)
    ### Motivation
    Keep all dependencies up-to date
    
    ### Modification
    Updated all deps to the latest version
    
    ### Result
    All deps are up-to date

--- build.gradle.kts
+++ build.gradle.kts
@@ -120,6 +120,13 @@ subprojects {
     toolVersion = rootProject.libs.versions.checkstyleTools.get()
   }
 
+  // checkstyle issue https://github.com/checkstyle/checkstyle/issues/14211
+  configurations.named("checkstyle") {
+    resolutionStrategy.capabilitiesResolution.withCapability("com.google.collections:google-collections") {
+      select("com.google.guava:guava:0")
+    }
+  }
+
   extensions.configure<SpotlessExtension> {
     java {
       licenseHeaderFile(rootProject.file("LICENSE_HEADER"))
