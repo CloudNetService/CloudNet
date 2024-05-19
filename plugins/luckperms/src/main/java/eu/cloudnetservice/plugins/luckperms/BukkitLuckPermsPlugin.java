3fc4b2a2a4681748890c2d66d7783ab25f7270dd
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2024-05-19 18:06:01 +0800

    feat: add luckperms plugin for cloudnet context support (#1258)
    <!-- 
    Thanks for taking your time and creating a pull request. Please note
    that we will not merge pull requests
    which are not following our code style
    (https://google.github.io/styleguide/javaguide.html). Most of these
    rules are checked while compile using checkstyle. On the other hand,
    please cover relevant code with tests.
    These are showing the maintainers what to expect from your pull requests
    and ensures that changes to your
    code will be consistent over time. See for example
    https://betterprogramming.pub/13-tips-for-writing-useful-unit-tests-ca20706b5368
    if you need a bit of guidance while writing your tests.
    -->
    
    ### Motivation
    In PR #1253 CloudPerms was removed, making LuckPerms one of the best
    alternatives
    But it lacks the feature of service / task exclusive permission 
    Therefor an optional plugin was made for those who want to use LuckPerms
    and use task exclusive permissions
    
    ### Modification
    Added a new plugin named CloudNet-LuckPerms-Addon to add some LuckPerms
    context (explained below)
    
    ### Result
    
    ![image](https://github.com/CloudNetService/CloudNet-v3/assets/36107150/c0d9043a-b07d-4ce6-a15a-5f4f6ea5f9d7)
    Four types of context were added: `task`, `service`, `environment` and
    `node`
    Server admins may then setup context in their permissions if needed, to
    make a permission service / task exclusive
    (Note: I only made the plugins for platforms which both CloudNet and
    LuckPerms support, and this has been tested on Paper &
    Velocity)
    
    ##### Other context
    N/A
    
    ---------
    
    Co-authored-by: RobotHanzo <RobotHanzo@users.noreply.github.com>
    Co-authored-by: 0utplay <aldin@sijamhodzic.de>

---
+++ plugins/luckperms/src/main/java/eu/cloudnetservice/plugins/luckperms/BukkitLuckPermsPlugin.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019-2024 CloudNetService team & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cloudnetservice.plugins.luckperms;
+
+import eu.cloudnetservice.ext.platforminject.api.PlatformEntrypoint;
+import eu.cloudnetservice.ext.platforminject.api.stereotype.Dependency;
+import eu.cloudnetservice.ext.platforminject.api.stereotype.PlatformPlugin;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import lombok.NonNull;
+import net.luckperms.api.LuckPermsProvider;
+
+@Singleton
+@PlatformPlugin(
+  platform = "bukkit",
+  name = "CloudNet-LuckPerms",
+  authors = "CloudNetService",
+  version = "@version@",
+  description = "Brings LuckPerms support to all server platforms",
+  pluginFileNames = "plugin.yml",
+  dependencies = @Dependency(name = "LuckPerms")
+)
+public class BukkitLuckPermsPlugin implements PlatformEntrypoint {
+
+  private final CloudNetContextCalculator cloudNetContextCalculator;
+
+  @Inject
+  public BukkitLuckPermsPlugin(@NonNull CloudNetContextCalculator cloudNetContextCalculator) {
+    this.cloudNetContextCalculator = cloudNetContextCalculator;
+  }
+
+  @Override
+  public void onLoad() {
+    LuckPermsProvider.get().getContextManager().registerCalculator(this.cloudNetContextCalculator);
+  }
+}
