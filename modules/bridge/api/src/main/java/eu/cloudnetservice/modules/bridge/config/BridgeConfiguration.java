860999f28e9de5a2827cb9acecf9df3566ae482f
Aldin <aldin@sijamhodzic.de>
2025-04-01 10:59:52 +0200

    feat: allow to include disconnect reason in bridge messages (#1399)
    ### Motivation
    When players are kicked from e.g. a Lobby service, the message the Lobby
    kicked them with is lost. This is a problem when having issues with
    version mismatches or plugins kicking someone from a fallback as the
    kick message is lost there too.
    
    ### Modification
    When getting kicked from a service and no other fallback is available
    the player will be kicked with the kick message of the downstream
    service. If there is no kick message, the newly introduced
    "server-kick-no-other-hub" message will be used.
    
    ### Result
    Kick reasons are properly usable in disconnect messages.
    
    ---------
    
    Co-authored-by: Pasqual Koschmieder <git@derklaro.dev>

--- modules/bridge/api/src/main/java/eu/cloudnetservice/modules/bridge/config/BridgeConfiguration.java
+++ modules/bridge/api/src/main/java/eu/cloudnetservice/modules/bridge/config/BridgeConfiguration.java
@@ -47,6 +47,7 @@ public final class BridgeConfiguration {
       .put("proxy-join-cancel-because-permission", "§7You do not have the required permissions to join this proxy.")
       .put("proxy-join-cancel-because-maintenance", "§7This proxy is currently in maintenance mode.")
       .put("proxy-join-disconnect-because-no-hub", "§cThere is currently no hub server you can connect to.")
+      .put("server-kick-no-other-hub", "§cThere is currently no hub server you can connect to.")
       .put("command-cloud-sub-command-no-permission", "§7You are not allowed to use §b%command%.")
       .put("already-connected", "§cYou are already connected to this network!")
       .put("error-connecting-to-server", "§cUnable to connect to %server%: %reason%")
--- modules/bridge/impl/src/main/java/eu/cloudnetservice/modules/bridge/impl/platform/velocity/VelocityPlayerManagementListener.java
+++ modules/bridge/impl/src/main/java/eu/cloudnetservice/modules/bridge/impl/platform/velocity/VelocityPlayerManagementListener.java
@@ -16,7 +16,6 @@
 
 package eu.cloudnetservice.modules.bridge.impl.platform.velocity;
 
-import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.ResultedEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
@@ -38,10 +37,6 @@ import jakarta.inject.Singleton;
 import java.util.Locale;
 import lombok.NonNull;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.TranslatableComponent;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import net.kyori.adventure.translation.GlobalTranslator;
 
 @Singleton
 public final class VelocityPlayerManagementListener {
@@ -64,10 +59,9 @@ public final class VelocityPlayerManagementListener {
     this.management = management;
   }
 
-  @Subscribe
+  @Subscribe(priority = Short.MAX_VALUE)
   public void handleLogin(@NonNull LoginEvent event) {
     var task = this.management.selfTask();
-    // check if the current task is present
     if (task != null) {
       // check if maintenance is activated
       if (task.maintenance() && !event.getPlayer().hasPermission("cloudnet.bridge.maintenance")) {
@@ -78,6 +72,7 @@ public final class VelocityPlayerManagementListener {
           component -> event.setResult(ResultedEvent.ComponentResult.denied(component)));
         return;
       }
+
       // check if a custom permission is required to join
       var permission = task.propertyHolder().getString("requiredPermission");
       if (permission != null && !event.getPlayer().hasPermission(permission)) {
@@ -89,71 +84,79 @@ public final class VelocityPlayerManagementListener {
         return;
       }
     }
+
     // check if the player is allowed to log in
-    var loginResult = this.proxyPlatformHelper.sendChannelMessagePreLogin(
-      this.management.createPlayerInformation(event.getPlayer()));
+    var playerInfo = this.management.createPlayerInformation(event.getPlayer());
+    var loginResult = this.proxyPlatformHelper.sendChannelMessagePreLogin(playerInfo);
     if (!loginResult.permitLogin()) {
       event.setResult(ResultedEvent.ComponentResult.denied(loginResult.result()));
     }
   }
 
-  @Subscribe(order = PostOrder.FIRST)
+  @Subscribe(priority = Short.MAX_VALUE)
   public void handleInitialServerChoose(@NonNull PlayerChooseInitialServerEvent event) {
-    // filter the next fallback
-    event.setInitialServer(this.management.fallback(event.getPlayer())
+    this.management.fallback(event.getPlayer())
       .flatMap(service -> this.proxyServer.getServer(service.name()))
-      .orElse(null));
-  }
-
-  @Subscribe(order = PostOrder.FIRST)
-  public void handleServerKick(@NonNull KickedFromServerEvent event) {
-    // check if the player is still active
-    if (event.getPlayer().isActive()) {
-      event.setResult(this.management.fallback(event.getPlayer(), event.getServer().getServerInfo().getName())
-        .flatMap(service -> this.proxyServer.getServer(service.name()))
-        .map(server -> {
-          // only notify the player if the fallback is the server the player is connected to
-          var curServer = event.getPlayer().getCurrentServer().map(ServerConnection::getServerInfo).orElse(null);
-          if (event.kickedDuringServerConnect() && curServer != null && curServer.equals(server.getServerInfo())) {
-            // send the player a nice message - velocity will keep the connection to the current server
-            // therefore we need to reset the fallback profile as no ServerPostConnectEvent will be called
-            this.management.handleFallbackConnectionSuccess(event.getPlayer());
-            return KickedFromServerEvent.Notify.create(this.extractReasonComponent(event));
-          } else {
-            // redirect the player to the next available hub server
-            return KickedFromServerEvent.RedirectPlayer.create(server, this.extractReasonComponent(event));
-          }
-        })
-        .orElse(KickedFromServerEvent.DisconnectPlayer.create(this.management.configuration().findMessage(
+      .ifPresentOrElse(event::setInitialServer, () -> {
+        var kickMessage = this.management.configuration().findMessage(
           event.getPlayer().getEffectiveLocale(),
           "proxy-join-disconnect-because-no-hub",
           ComponentFormats.BUNGEE_TO_ADVENTURE::convert,
-          Component.empty(),
-          true))));
+          null,
+          true);
+        if (kickMessage != null) {
+          event.getPlayer().disconnect(kickMessage);
+        }
+      });
+  }
+
+  @Subscribe(priority = Short.MAX_VALUE)
+  public void handleServerKick(@NonNull KickedFromServerEvent event) {
+    var player = event.getPlayer();
+    if (player.isActive()) {
+      var kickResult = this.management.fallback(player, event.getServer().getServerInfo().getName())
+        .flatMap(service -> this.proxyServer.getServer(service.name()))
+        .map(fallback -> {
+          var kickReason = this.buildKickReasonMessage(event, "error-connecting-to-server");
+          var prevServer = player.getCurrentServer().map(ServerConnection::getServerInfo).orElse(null);
+          if (event.kickedDuringServerConnect() && prevServer != null && prevServer.equals(fallback.getServerInfo())) {
+            // send the player a nice message - velocity will keep the connection to the current server
+            // therefore we need to reset the fallback profile as no ServerPostConnectEvent will be called
+            this.management.handleFallbackConnectionSuccess(player);
+            return KickedFromServerEvent.Notify.create(kickReason);
+          } else {
+            // redirect the player to the next available hub server
+            return KickedFromServerEvent.RedirectPlayer.create(fallback, kickReason);
+          }
+        })
+        .orElseGet(() -> {
+          // no fallback available, disconnect the player
+          var kickReason = this.buildKickReasonMessage(event, "server-kick-no-other-hub");
+          return KickedFromServerEvent.DisconnectPlayer.create(kickReason);
+        });
+      event.setResult(kickResult);
     }
   }
 
   @Subscribe
   public void handleServiceConnected(@NonNull ServerPostConnectEvent event) {
-    var joinedServiceInfo = event.getPlayer().getCurrentServer()
+    var player = event.getPlayer();
+    var joinedServiceInfo = player.getCurrentServer()
       .flatMap(server -> this.management
         .cachedService(service -> server.getServerInfo().getName().equals(service.name()))
         .map(NetworkServiceInfo::fromServiceInfoSnapshot))
       .orElse(null);
-    // check if the connection was initial
     if (event.getPreviousServer() == null) {
       // the player logged in successfully if he is now connected to a service for the first time
-      this.proxyPlatformHelper.sendChannelMessageLoginSuccess(
-        this.management.createPlayerInformation(event.getPlayer()),
-        joinedServiceInfo);
-      // update the service info
+      var playerInfo = this.management.createPlayerInformation(player);
+      this.proxyPlatformHelper.sendChannelMessageLoginSuccess(playerInfo, joinedServiceInfo);
       this.serviceInfoHolder.publishServiceInfoUpdate();
     } else if (joinedServiceInfo != null) {
       // the player switched the service
-      this.proxyPlatformHelper.sendChannelMessageServiceSwitch(event.getPlayer().getUniqueId(), joinedServiceInfo);
+      this.proxyPlatformHelper.sendChannelMessageServiceSwitch(player.getUniqueId(), joinedServiceInfo);
     }
-    // notify the management that the player successfully connected to a service
-    this.management.handleFallbackConnectionSuccess(event.getPlayer());
+
+    this.management.handleFallbackConnectionSuccess(player);
   }
 
   @Subscribe
@@ -164,48 +167,32 @@ public final class VelocityPlayerManagementListener {
     if (status == DisconnectEvent.LoginStatus.SUCCESSFUL_LOGIN
       || status == DisconnectEvent.LoginStatus.PRE_SERVER_JOIN) {
       this.proxyPlatformHelper.sendChannelMessageDisconnected(event.getPlayer().getUniqueId());
-      // update the service info
       this.serviceInfoHolder.publishServiceInfoUpdate();
     }
-    // always remove the player fallback profile
+
     this.management.removeFallbackProfile(event.getPlayer());
   }
 
-  private @NonNull Component extractReasonComponent(@NonNull KickedFromServerEvent event) {
+  private @NonNull Component buildKickReasonMessage(@NonNull KickedFromServerEvent event, @NonNull String messageKey) {
     var playerLocale = event.getPlayer().getEffectiveLocale();
-    var message = event.getServerKickReason().orElse(null);
-    // use the current result if it is Notify - velocity already created a friendly reason for us
-    if (message == null && event.getResult() instanceof KickedFromServerEvent.Notify notify) {
-      message = notify.getMessageComponent();
-    }
-    // check if we have a reason component
-    if (message != null) {
-      // check if we should try to translate the message
-      if (message instanceof TranslatableComponent) {
-        message = GlobalTranslator.render(message, playerLocale == null ? Locale.getDefault() : playerLocale);
-      }
-      // if the message is still not a TextComponent use the default message instead
-      if (message instanceof TextComponent textComponent) {
-        // wrap the message
-        return this.management.configuration().findMessage(
-          playerLocale,
-          "error-connecting-to-server",
-          rawMessage -> ComponentFormats.BUNGEE_TO_ADVENTURE.convert(rawMessage
-            .replace("%server%", event.getServer().getServerInfo().getName())
-            .replace("%reason%", LegacyComponentSerializer.legacySection().serialize(textComponent))),
-          Component.empty(),
-          true);
-      }
-    }
+    var serverName = event.getServer().getServerInfo().getName();
+    var reason = event.getServerKickReason().orElseGet(() -> Component.text("Disconnected by Server"));
 
-    // unknown reason
-    return this.management.configuration().findMessage(
+    var rawReasonMessage = this.management.configuration().findMessage(
       playerLocale,
-      "error-connecting-to-server",
-      rawMessage -> ComponentFormats.BUNGEE_TO_ADVENTURE.convert(rawMessage
-        .replace("%server%", event.getServer().getServerInfo().getName())
-        .replace("%reason%", "§cUnknown")),
-      Component.empty(),
+      messageKey,
+      ComponentFormats.BUNGEE_TO_ADVENTURE::convert,
+      null,
       true);
+    if (rawReasonMessage == null) {
+      // if no message is configured in the bridge config, fall back to the kick
+      // reason provided by the server, as we do need a message to prevent a
+      // possible disconnection by the velocity
+      return reason;
+    } else {
+      return rawReasonMessage
+        .replaceText(builder -> builder.matchLiteral("%reason%").replacement(reason))
+        .replaceText(builder -> builder.matchLiteral("%server%").replacement(serverName));
+    }
   }
 }
