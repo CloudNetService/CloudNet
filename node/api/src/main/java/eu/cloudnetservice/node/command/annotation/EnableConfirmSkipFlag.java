adbe832b8864908c17b9161c2ff69395d6527273
Aldin <aldin@sijamhodzic.de>
2025-06-07 21:08:55 +0200

    feat: make command confirmations skippable using extra flag (#1645)
    ### Motivation
    In #1579 it was requested to make command confirmations (like needed for
    `exit`) skippable using a flag.
    
    ### Modification
    Added a new `@SkipConfirmation` annotation which allows to specify a
    flag name which can be used to skip the confirmation of a command. This
    PR also directly includes this annotation for the exit command so that
    it can be skipped using `--skip`
    
    ### Result
    Commands that need to be confirmed can be skipped if requested.
    
    ##### Other context
    Resolves #1579

---
+++ node/api/src/main/java/eu/cloudnetservice/node/command/annotation/EnableConfirmSkipFlag.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019-2024 CloudNetService team & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cloudnetservice.node.command.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import lombok.NonNull;
+
+/**
+ * This annotation allows users to skip the required confirmation
+ * {@link org.incendo.cloud.processors.confirmation.annotation.Confirmation} of a command. When this annotation is
+ * applied to a method a flag is implicitly appended to the command which can be used to skip the confirmation.
+ * <p>
+ * The annotations value is used as the flag name, which defaults to {@code confirm}.
+ *
+ * @see org.incendo.cloud.processors.confirmation.annotation.Confirmation
+ * @since 4.0
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnableConfirmSkipFlag {
+
+  /**
+   * The value of this annotation is used as the flag name to skip the confirmation. Should not include the leading
+   * dashes.
+   * <p>
+   * Defaults to {@code confirm}, allowing users to skip the confirmation by appending {@code --confirm}.
+   *
+   * @return the flag name that allows skipping.
+   */
+  @NonNull String value() default "confirm";
+}
