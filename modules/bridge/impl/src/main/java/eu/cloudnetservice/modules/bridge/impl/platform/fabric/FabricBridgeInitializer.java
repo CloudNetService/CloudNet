0556ba7baec556f884d3badd0cfc4fbca4d62889
aldin@sijamhodzic.de <aldin@sijamhodzic.de>
2025-06-24 09:30:30 +0200

    chore: bump fabric comp. version

--- modules/bridge/impl/src/main/java/eu/cloudnetservice/modules/bridge/impl/platform/fabric/FabricBridgeInitializer.java
+++ modules/bridge/impl/src/main/java/eu/cloudnetservice/modules/bridge/impl/platform/fabric/FabricBridgeInitializer.java
@@ -31,7 +31,7 @@ import lombok.NonNull;
   version = "@version@",
   dependencies = {
     @Dependency(name = "fabricloader", version = ">=0.16.6"),
-    @Dependency(name = "minecraft", version = "~1.21"),
+    @Dependency(name = "minecraft", version = "~1.21.6"),
     @Dependency(name = "java", version = "24")
   },
   authors = "CloudNetService"
