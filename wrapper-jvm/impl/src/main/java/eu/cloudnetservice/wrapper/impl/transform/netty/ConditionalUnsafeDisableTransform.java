33714dc3a568c5e2224471f91946f787e23483f7
Pasqual Koschmieder <git@derklaro.dev>
2025-06-28 10:46:39 +0200

    improve disabling to handle multiple netty versions on the classpath

--- wrapper-jvm/impl/src/main/java/eu/cloudnetservice/wrapper/impl/transform/netty/ConditionalUnsafeDisableTransform.java
+++ wrapper-jvm/impl/src/main/java/eu/cloudnetservice/wrapper/impl/transform/netty/ConditionalUnsafeDisableTransform.java
@@ -16,9 +16,13 @@
 
 package eu.cloudnetservice.wrapper.impl.transform.netty;
 
-import eu.cloudnetservice.wrapper.transform.ClassTransformer;
+import eu.cloudnetservice.wrapper.impl.transform.ClassTransformer;
 import java.lang.classfile.ClassModel;
 import java.lang.classfile.ClassTransform;
+import java.lang.classfile.MethodModel;
+import java.lang.constant.ClassDesc;
+import java.lang.constant.ConstantDescs;
+import java.lang.constant.MethodTypeDesc;
 import lombok.NonNull;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
@@ -26,7 +30,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Transformer to disable the use of unsafe in netty when the {@code CleanerJava24Linker} (introduced in netty 4.2.3) is
  * available. This should result in a similar performance than the use of unsafe, without using the deprecated
- * memory-access methods in unsafe.
+ * memory-access methods in {@code sun.misc.Unsafe}.
  *
  * @since 4.0
  */
@@ -34,8 +38,15 @@ import org.jetbrains.annotations.Nullable;
 public final class ConditionalUnsafeDisableTransform implements ClassTransformer {
 
   private static final String SYS_PROP_NO_UNSAFE = "io.netty.noUnsafe";
-  private static final String CNI_PLATFORM_DEPENDENT0 = "io/netty/util/internal/PlatformDependent0";
-  private static final String CNI_CLEANER_LINKER_JAVA24 = "io/netty/util/internal/CleanerJava24Linker";
+  private static final String CN_PLATFORM_DEPENDENT0 = "PlatformDependent0";
+  private static final String CN_CLEANER_LINKER_JAVA24 = "CleanerJava24Linker";
+
+  private static final String MN_EXPLICIT_NO_UNSAFE_CAUSE = "explicitNoUnsafeCause0";
+  private static final MethodTypeDesc MTD_EXPLICIT_NO_UNSAFE_CAUSE = MethodTypeDesc.of(ConstantDescs.CD_Throwable);
+
+  private static final ClassDesc CD_UNSUPPORTED_OP_EX = ClassDesc.of(UnsupportedOperationException.class.getName());
+  private static final MethodTypeDesc MTD_UNSUPPORTED_OP_EX_NEW =
+    MethodTypeDesc.of(ConstantDescs.CD_void, ConstantDescs.CD_String);
 
   /**
    * Constructs a new instance of this transformer, usually done via SPI.
@@ -65,12 +76,27 @@ public final class ConditionalUnsafeDisableTransform implements ClassTransformer
     // check if the CleanerJava24Linker class is available, in which case we want to prefer
     // this cleaner implementation over unsafe, so we disable the use of unsafe in netty. this
     // might come with some side effects for other libs. therefore, the transformer can be disabled.
+    var packageName = original.thisClass().asSymbol().packageName().replace('.', '/');
+    var cleanerJava24LinkerClassName = String.format("%s/%s.class", packageName, CN_CLEANER_LINKER_JAVA24);
     var cleanerLinkerResource = switch (loader) {
-      case ClassLoader cl -> cl.getResource(CNI_CLEANER_LINKER_JAVA24 + ".class");
-      case null -> ClassLoader.getSystemResource(CNI_CLEANER_LINKER_JAVA24 + ".class");
+      case ClassLoader cl -> cl.getResource(cleanerJava24LinkerClassName);
+      case null -> ClassLoader.getSystemResource(cleanerJava24LinkerClassName);
     };
     if (cleanerLinkerResource != null) {
-      System.setProperty(SYS_PROP_NO_UNSAFE, "true");
+      return (builder, element) -> {
+        if (element instanceof MethodModel mm
+          && mm.methodName().equalsString(MN_EXPLICIT_NO_UNSAFE_CAUSE)
+          && mm.methodTypeSymbol().equals(MTD_EXPLICIT_NO_UNSAFE_CAUSE)) {
+          builder.withMethodBody(mm.methodName(), mm.methodType(), mm.flags().flagsMask(), code -> code
+            .new_(CD_UNSUPPORTED_OP_EX)
+            .dup()
+            .ldc("Unsafe auto-disabled on J24+ by CloudNet when linker-based cleaner is available")
+            .invokespecial(CD_UNSUPPORTED_OP_EX, ConstantDescs.INIT_NAME, MTD_UNSUPPORTED_OP_EX_NEW)
+            .areturn());
+        } else {
+          builder.with(element);
+        }
+      };
     }
 
     return ClassTransform.ACCEPT_ALL;
@@ -82,7 +108,7 @@ public final class ConditionalUnsafeDisableTransform implements ClassTransformer
   @Override
   public @NonNull TransformWillingness classTransformWillingness(@NonNull String internalClassName) {
     // need to decide only once for the entire lifetime of the jvm
-    var isPlatformDependent0 = internalClassName.equals(CNI_PLATFORM_DEPENDENT0);
-    return isPlatformDependent0 ? TransformWillingness.ACCEPT_ONCE : TransformWillingness.REJECT;
+    var isPlatformDependent0 = internalClassName.endsWith(CN_PLATFORM_DEPENDENT0);
+    return isPlatformDependent0 ? TransformWillingness.ACCEPT : TransformWillingness.REJECT;
   }
 }
