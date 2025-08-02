473a7c44fa81ffa9c7e5026ecaac4fa278ff8c9d
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-08-02 12:54:55 +0200

    remove indra

--- build-extensions/src/main/kotlin/extensions.kt
+++ build-extensions/src/main/kotlin/extensions.kt
@@ -14,26 +14,18 @@
  * limitations under the License.
  */
 
-import net.kyori.indra.git.IndraGitExtension
-import net.kyori.indra.git.RepositoryValueSource
-import net.kyori.indra.git.internal.IndraGitExtensionImpl
-import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.lib.Constants
+import git.GitExtension
+import git.GitService
 import org.gradle.api.Project
-import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
-import org.gradle.api.internal.artifacts.repositories.resolver.MavenUniqueSnapshotComponentIdentifier
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.attributes
 import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.the
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.util.logging.Level
-import java.util.logging.Logger
 
 fun Project.applyJarMetadata(mainClass: String, module: String) {
   applyJarMetadata(mainClass, module, null)
@@ -42,49 +34,34 @@ fun Project.applyJarMetadata(mainClass: String, module: String) {
 fun Project.applyJarMetadata(mainClass: String, module: String, preMain: String?) {
   if ("jar" in tasks.names) {
     tasks.named<Jar>("jar") {
+      val service = git()?.service
+      service?.let { usesService(it) }
+
       manifest.attributes(
         "Main-Class" to mainClass,
         "Automatic-Module-Name" to module,
         "Implementation-Vendor" to "CloudNetService",
         "Implementation-Title" to Versions.cloudNetCodeName,
-        "Implementation-Version" to project.version.toString() + "-${shortCommitHash()}")
+        "Implementation-Version" to project.version.toString() + "-${service.shortCommitHash()}"
+      )
       // apply the pre-main class if given
       if (preMain != null) {
         manifest.attributes("Premain-Class" to preMain)
       }
       // apply git information to manifest
-      git()?.let { git ->
-        val commit = git.commit().map { it.name.substring(0, 8) }
-        val branchName = git.repositoryValue(BranchName::class.java)
-        if (commit.isPresent) manifest.attributes(IndraGitExtension.MANIFEST_ATTRIBUTE_GIT_COMMIT to commit.get())
-        if (branchName.isPresent) manifest.attributes(IndraGitExtension.MANIFEST_ATTRIBUTE_GIT_BRANCH to branchName.get())
+      service?.run { get() }?.let { service ->
+        service.commit?.name?.substring(0, 8)?.let { manifest.attributes("Git-Commit" to it) }
+        service.branchName?.let { manifest.attributes("Git-Branch" to it) }
       }
     }
   }
 }
 
-/**
- * Indra does not properly support configuration caching for #branchName yet
- */
-private abstract class BranchName : RepositoryValueSource.Parameterless<String>() {
-  override fun obtain(repository: Git): String? {
-    try {
-      val ref = repository.repository.exactRef(Constants.HEAD)
-      if (ref == null || !ref.isSymbolic) return null // no HEAD, or detached HEAD
-
-      return ref.target.name
-    } catch (ex: IOException) {
-      Logger.getGlobal().log(Level.SEVERE, "Failed to query current branch name from git:", ex)
-      return null
-    }
-  }
+fun Provider<GitService>?.shortCommitHash(): String {
+  return this?.get()?.commit?.name()?.substring(0, 8) ?: "unknown"
 }
 
-fun Project.shortCommitHash(): String {
-  return git()?.commit()?.get()?.name()?.substring(0, 8) ?: "unknown"
-}
-
-fun Project.git(): IndraGitExtension? = extensions.findByType()
+fun Project.git(): GitExtension? = extensions.findByType()
 
 fun Project.sourceSets(): SourceSetContainer = the<JavaPluginExtension>().sourceSets
 
---
+++ build-extensions/src/main/kotlin/git/GitService.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2019-2025 CloudNetService team & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package git
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.*
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.services.BuildService
+import org.gradle.api.services.BuildServiceParameters
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.Closeable
+import java.lang.AutoCloseable
+import java.nio.file.Path
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.io.path.isDirectory
+import kotlin.io.path.notExists
+import kotlin.io.path.useLines
+
+abstract class GitService : BuildService<GitService.Params>, AutoCloseable {
+  private val git: GitInstance = GitInstance(parameters.projectDirectory.get().asFile.toPath())
+
+  override fun close() {
+  }
+
+  interface Params : BuildServiceParameters {
+    val projectDirectory: DirectoryProperty
+  }
+
+  val branchName: String?
+    get() = branch?.run { Repository.shortenRefName(this.name) }
+
+  val commit: ObjectId?
+    get() = head?.run { this.objectId }
+
+  val head: Ref?
+    get() = git.git?.repository?.run { exactRef(Constants.HEAD) }
+
+  val branch: Ref?
+    get() = head?.run { if (!this.isSymbolic) null else this.target }
+
+  /**
+   * Currently has no support for git submodules.
+   * We don't use them so it should be ok
+   */
+  private class GitInstance(dir: Path) : Closeable {
+    companion object {
+      val logger: Logger = LoggerFactory.getLogger(GitInstance::class.java)
+      const val GIT_DIR = ".git"
+      const val GITDIR_PREFIX = "gitdir:"
+    }
+
+    private val usable = AtomicBoolean(false)
+    val repository: Repository?
+    val git: Git?
+
+    init {
+      var targetDir: Path? = dir
+      while (targetDir?.resolve(GIT_DIR)?.notExists() ?: false) {
+        targetDir = targetDir.parent
+      }
+
+      val gitDir = targetDir?.let { resolveGitDirectory(it) }
+      if (targetDir == null || gitDir == null) {
+        logger.info("[Git] Unable to find repository for $dir")
+        repository = null
+        git = null
+      } else {
+        repository =
+          RepositoryBuilder().setWorkTree(dir.toFile()).setGitDir(gitDir.toFile()).setMustExist(true).build()
+        git = Git.wrap(repository)
+      }
+    }
+
+    private fun resolveGitDirectory(projectDir: Path): Path? {
+      // https://git-scm.com/docs/gitrepository-layout
+      // .git file is allowed with 'gitdir:' reference
+      projectDir.run {
+        if (fileName.toString() == GIT_DIR) projectDir else projectDir.resolve(GIT_DIR)
+      }.run {
+        if (isDirectory()) return this
+        this.useLines { sequence ->
+          sequence.forEach { line ->
+            if (line.startsWith(GITDIR_PREFIX)) {
+              return parent.resolve(line.substring(GITDIR_PREFIX.length))
+            }
+          }
+        }
+        logger.warn("[Git] Could not determine git directory for $projectDir")
+        return null
+      }
+    }
+
+    override fun close() {
+      if (usable.compareAndSet(true, false)) {
+        repository!!.close()
+      }
+    }
+  }
+}
