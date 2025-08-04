a8f4745e22f80a860df90b542cf2c030c6a27e45
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-08-04 21:25:20 +0000

    update foojay resolver convention to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ pluginManagement {
   }
 }
 plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
   id("com.gradle.develocity") version ("4.1")
   id("cloudnet-settings")
 }
