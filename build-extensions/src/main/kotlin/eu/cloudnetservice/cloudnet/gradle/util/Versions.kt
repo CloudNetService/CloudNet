c924e5ae8b619b6f0b71c6ed68b443280e8d17e2
0utplay <aldin@sijamhodzic.de>
2026-01-11 20:42:17 +0100

    chore: release version 4.0.0-RC16

--- build-extensions/src/main/kotlin/eu/cloudnetservice/cloudnet/gradle/util/Versions.kt
+++ build-extensions/src/main/kotlin/eu/cloudnetservice/cloudnet/gradle/util/Versions.kt
@@ -17,6 +17,6 @@
 package eu.cloudnetservice.cloudnet.gradle.util
 
 object Versions {
-  const val CLOUDNET = "4.0.0-RC16-SNAPSHOT"
+  const val CLOUDNET = "4.0.0-RC16"
   const val CLOUDNET_CODE_NAME = "Blizzard"
 }
