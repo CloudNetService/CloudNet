38f995dcbab33636938e1448f32f12ba164277ac
0utplay <aldin@sijamhodzic.de>
2026-01-29 17:11:40 +0100

    fix: improve paperclip indicator checks to support e.g. leafmc

--- wrapper-jvm/impl/src/main/java/eu/cloudnetservice/wrapper/impl/transform/bukkit/PaperclipClassLoaderTransformer.java
+++ wrapper-jvm/impl/src/main/java/eu/cloudnetservice/wrapper/impl/transform/bukkit/PaperclipClassLoaderTransformer.java
@@ -20,8 +20,10 @@ import eu.cloudnetservice.wrapper.impl.transform.ClassTransformer;
 import java.lang.classfile.ClassModel;
 import java.lang.classfile.ClassTransform;
 import java.lang.classfile.instruction.InvokeInstruction;
+import java.lang.reflect.AccessFlag;
 import lombok.NonNull;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * A transformer implementation that removes the paperclip call to get the parent class loader of the paperclip class
@@ -30,18 +32,34 @@ import org.jetbrains.annotations.ApiStatus;
  * @since 4.0
  */
 @ApiStatus.Internal
-public class PaperclipClassLoaderTransformer implements ClassTransformer {
+public final class PaperclipClassLoaderTransformer implements ClassTransformer {
 
   private static final String MAIN_METHOD_NAME = "main";
   private static final String CLASS_LOADER_GET_PARENT_METHOD = "getParent";
   private static final String CLASS_LOADER_INTERNAL_NAME = "java/lang/ClassLoader";
-  private static final String PAPERCLIP_MAIN_CLASS = "io/papermc/paperclip/Paperclip";
+  private static final String PAPERCLIP_INDICATOR_METHOD = "extractAndApplyPatches";
+
+  private final String mainClassPackage;
 
   /**
    * Constructs a new instance of this transformer, usually done via SPI.
    */
   public PaperclipClassLoaderTransformer() {
-    // used by SPI
+    var versionListPresent = ClassLoader.getSystemClassLoader().getResource("META-INF/versions.list") != null;
+    this.mainClassPackage = versionListPresent ? findMainClassPackage() : null;
+  }
+
+  private static @Nullable String findMainClassPackage() {
+    var command = System.getProperty("sun.java.command");
+    if (command != null) {
+      var mainClassName = command.split("\\s+", 2)[0].replace('.', '/');
+      var lastSlashIndex = mainClassName.lastIndexOf('/');
+      if (lastSlashIndex != -1) {
+        return mainClassName.substring(0, lastSlashIndex);
+      }
+    }
+
+    return null;
   }
 
   /**
@@ -49,9 +67,16 @@ public class PaperclipClassLoaderTransformer implements ClassTransformer {
    */
   @Override
   public @NonNull TransformWillingness classTransformWillingness(@NonNull String internalClassName) {
-    return internalClassName.equals(PAPERCLIP_MAIN_CLASS)
-      ? TransformWillingness.ACCEPT_ONCE
-      : TransformWillingness.REJECT;
+    if (this.mainClassPackage == null) {
+      return TransformWillingness.REJECT;
+    }
+
+    var lastSlashIndex = internalClassName.lastIndexOf('/');
+    if (lastSlashIndex == this.mainClassPackage.length() && internalClassName.startsWith(this.mainClassPackage)) {
+      return TransformWillingness.ACCEPT;
+    }
+
+    return TransformWillingness.REJECT;
   }
 
   /**
@@ -59,8 +84,16 @@ public class PaperclipClassLoaderTransformer implements ClassTransformer {
    */
   @Override
   public @NonNull ClassTransform provideClassTransform(@NonNull ClassModel original) {
+    var indicatorMethod = original.methods()
+      .stream()
+      .anyMatch(method -> method.methodName().equalsString(PAPERCLIP_INDICATOR_METHOD));
+    if (!indicatorMethod) {
+      return ClassTransform.ACCEPT_ALL;
+    }
+
     return ClassTransform.transformingMethodBodies(
-      methodModel -> methodModel.methodName().equalsString(MAIN_METHOD_NAME),
+      methodModel -> methodModel.methodName().equalsString(MAIN_METHOD_NAME)
+        && methodModel.flags().has(AccessFlag.STATIC),
       (builder, element) -> {
         if (element instanceof InvokeInstruction invoke) {
           if (invoke.method().name().equalsString(CLASS_LOADER_GET_PARENT_METHOD)
