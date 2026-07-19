419d34e5d9967f228fa4311c09d420d428137148
Pasqual Koschmieder <git@derklaro.dev>
2026-07-19 21:38:11 +0200

    fix/improve

--- modules/npcs/impl/src/main/java/eu/cloudnetservice/modules/npc/impl/platform/bukkit/util/BukkitPacketAdapterUtil.java
+++ modules/npcs/impl/src/main/java/eu/cloudnetservice/modules/npc/impl/platform/bukkit/util/BukkitPacketAdapterUtil.java
@@ -21,7 +21,6 @@ import com.github.juliarn.npclib.bukkit.protocol.BukkitProtocolAdapter;
 import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.util.PEVersion;
 import eu.cloudnetservice.utils.base.StringUtil;
-import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.function.Supplier;
@@ -32,70 +31,91 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class BukkitPacketAdapterUtil {
 
-  private static final String NPC_PACKET_ADAPTER = System.getProperty("cloudnet.npcs.packet-adapter");
+  private static final Logger LOGGER = LoggerFactory.getLogger(BukkitPacketAdapterUtil.class);
 
-  private static final MethodHandle SERVER_GET_BUKKIT_VERSION;
-
-  static {
-    MethodHandle handle = null;
-    try {
-      handle = MethodHandles.publicLookup().findVirtual(Server.class, "getBukkitVersion", MethodType.methodType(String.class));
-    } catch (Throwable _) {
-    }
-    SERVER_GET_BUKKIT_VERSION = handle;
-  }
-
-  private static final Supplier<PlatformPacketAdapter<World, Player, ItemStack, Plugin>> RESOLVED = StableValue.supplier(
-    BukkitPacketAdapterUtil::resolveAdapter);
+  private static final String SYS_PROP_PACKET_ADAPTER = "cloudnet.npcs.packet-adapter";
+  private static final Supplier<PlatformPacketAdapter<World, Player, ItemStack, Plugin>> PACKET_ADAPTER_HOLDER =
+    StableValue.supplier(BukkitPacketAdapterUtil::resolveAdapter);
 
   private BukkitPacketAdapterUtil() {
     throw new UnsupportedOperationException();
   }
 
+  /**
+   * Resolves the packet adapter to use, either using a specific override or the Minecraft version of the server. This
+   * method lazily initializes the version and always returns the same value after the first invocation.
+   *
+   * @return the npc packet adapter to use.
+   */
   public static @NonNull PlatformPacketAdapter<World, Player, ItemStack, Plugin> resolve() {
-    return RESOLVED.get();
+    return PACKET_ADAPTER_HOLDER.get();
   }
 
+  /**
+   * Resolves the packet adapter to use, either using a specific override or the Minecraft version of the server.
+   *
+   * @return the npc packet adapter to use.
+   */
   private static @NonNull PlatformPacketAdapter<World, Player, ItemStack, Plugin> resolveAdapter() {
-    if (NPC_PACKET_ADAPTER == null) {
-      return evaluateAdapter();
-    }
-
-    return switch (StringUtil.toLower(NPC_PACKET_ADAPTER)) {
+    var overriddenAdapterName = System.getProperty(SYS_PROP_PACKET_ADAPTER);
+    return switch (StringUtil.toLower(overriddenAdapterName)) {
       case "protocollib" -> BukkitProtocolAdapter.protocolLib();
       case "packetevents" -> BukkitProtocolAdapter.packetEvents();
-      default -> evaluateAdapter();
+      case null, default -> evaluateAdapter();
     };
   }
 
+  /**
+   * Evaluates the packet adapter to use based on the current Minecraft version running on the server. If no reliable
+   * guess can be made, this method returns the {@code ProtocolLib} packet adapter implementation.
+   *
+   * @return the packet adapter to use based on the current Minecraft version running on the server.
+   */
   private static @NonNull PlatformPacketAdapter<World, Player, ItemStack, Plugin> evaluateAdapter() {
     try {
-      var bukkitVersion = resolveMinecraftVersion();
-      var versionStart = bukkitVersion.indexOf('-');
-      var parsedVersion = PEVersion.fromString(versionStart >= 0 ? bukkitVersion.substring(0, versionStart) : bukkitVersion);
-      var latestPEVersion = PEVersion.fromString(ServerVersion.getLatest().getReleaseName());
-      if (parsedVersion.isNewerThan(latestPEVersion)) {
-        return BukkitProtocolAdapter.protocolLib();
-      }
-
-      return BukkitProtocolAdapter.packetEvents();
-    } catch (Throwable _) {
+      var minecraftVersion = resolveMinecraftVersion();
+      var parsedMinecraftVersion = PEVersion.fromString(minecraftVersion);
+      var latestPeSupportedMinecraftVersion = PEVersion.fromString(ServerVersion.getLatest().getReleaseName());
+      var packetEventsNotSupported = parsedMinecraftVersion.isNewerThan(latestPeSupportedMinecraftVersion);
+      return packetEventsNotSupported ? BukkitProtocolAdapter.protocolLib() : BukkitProtocolAdapter.packetEvents();
+    } catch (Exception exception) {
+      LOGGER.warn(
+        "Failed to reliably determine supported packet adapter ({}: {}), falling back to ProtocolLib",
+        exception.getClass().getSimpleName(), exception.getMessage());
       return BukkitProtocolAdapter.protocolLib();
     }
   }
 
+  /**
+   * Resolves the Minecraft version currently running on the server.
+   *
+   * @return the Minecraft version currently running on the server.
+   * @throws RuntimeException if no reliable guess can be made about the running version.
+   */
   private static @NonNull String resolveMinecraftVersion() {
     try {
-      if (SERVER_GET_BUKKIT_VERSION != null) {
-        return (String) SERVER_GET_BUKKIT_VERSION.invoke(Bukkit.getServer());
-      }
+      var lookup = MethodHandles.publicLookup();
+      var getMinecraftVersion = lookup.findVirtual(
+        Server.class,
+        "getMinecraftVersion",
+        MethodType.methodType(String.class));
+      return (String) getMinecraftVersion.invokeExact(Bukkit.getServer());
     } catch (Throwable _) {
+      var bukkitVersion = Bukkit.getBukkitVersion();
+      var maybeMinecraftVersion = bukkitVersion.split("-", 2)[0];
+      if (maybeMinecraftVersion.indexOf('.') == -1) {
+        var msg = String.format(
+          "Failed to determine Minecraft version from '%s'. Use the system property '%s' to set a packet adapter",
+          bukkitVersion, SYS_PROP_PACKET_ADAPTER);
+        throw new RuntimeException(msg);
+      }
+
+      return maybeMinecraftVersion;
     }
-    return Bukkit.getBukkitVersion();
   }
 }
-
-
